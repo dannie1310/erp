3d63031abb61cd666180bd96c4a6b1d6f8db686e
Francisco Esquivel <jfesquivel@grupohi.mx>
2019-02-21 17:38:41 -0600

    CuentaCostoCreate vue component

---
+++ resources/js/components/contabilidad/cuenta-costo/Create.vue
@@ -0,0 +1,118 @@
+<template>
+    <span>
+        <button @click="init" v-if="$root.can('registrar_cuenta_costo')" class="btn btn-app btn-info pull-right">
+            <i class="fa fa-plus"></i> Registrar Cuenta
+        </button>
+
+        <div class="modal fade" ref="modal" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">REGISTRAR CUENTA DE COSTO</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form role="form" @submit.prevent="validate">
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group error-content">
+                                        <label for="id_costo">Costo</label>
+                                        <costo-select
+                                                name="id_costo"
+                                                data-vv-as="Costo"
+                                                v-validate="{required: true}"
+                                                id="id_costo"
+                                                v-model="id_costo"
+                                                :error="errors.has('id_costo')">
+                                        ></costo-select>
+                                        <div class="error-label" v-show="errors.has('id_costo')">{{ errors.first('id_costo') }}</div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group error-content">
+                                        <label for="cuenta">Cuenta</label>
+                                        <input
+                                                type="text"
+                                                name="cuenta"
+                                                data-vv-as="Cuenta"
+                                                v-validate="{required: true, regex: datosContables}"
+                                                class="form-control"
+                                                v-mask="{regex: datosContables}"
+                                                id="cuenta"
+                                                placeholder="Cuenta"
+                                                v-model="cuenta"
+                                                :class="{'is-invalid': errors.has('cuenta')}">
+                                        <div class="invalid-feedback" v-show="errors.has('cuenta')">{{ errors.first('cuenta') }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                            <button type="submit" class="btn btn-primary">Registrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </span>
+</template>
+
+<script>
+    import CostoSelect from "../../cadeco/costo/Select";
+    export default {
+        name: "cuenta-costo-create",
+        components: {CostoSelect},
+        data() {
+            return {
+                id_costo: '',
+                cuenta: ''
+            }
+        },
+
+        methods: {
+            init() {
+                $(this.$refs.modal).modal('show');
+
+                this.id_costo = '';
+                this.cuenta = '';
+
+                this.$validator.reset()
+            },
+
+            store() {
+                return this.$store.dispatch('contabilidad/cuenta-costo/store', this.$data)
+                    .then((data) => {
+                        $(this.$refs.modal).modal('hide');
+                        this.$emit('created', data);
+                    });
+            },
+
+            validate() {
+                this.$validator.validate().then(result => {
+                    if (result) {
+                        this.store()
+                    }
+                });
+            }
+        },
+
+        computed: {
+            datosContables() {
+                return this.$store.getters['auth/datosContables']
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .error-label {
+        width: 100%;
+        margin-top: 0.25rem;
+        font-size: 80%;
+        color: #dc3545;
+    }
+</style>
