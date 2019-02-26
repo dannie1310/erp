d62e9f57634d079b945f03084f2025da17c7ff65
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2019-02-25 20:09:03 -0600

    Backend necesario para enlistar los subcontratos que no tienen fondo de garantía

---
+++ app/Http/Controllers/v1/CADECO/Contratos/SubcontratoController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EMartinez
+ * Date: 25/02/2019
+ * Time: 07:05 PM
+ */
+
+namespace App\Http\Controllers\v1\CADECO\Contratos;
+
+
+class SubcontratoController
+{
+
+}
---
+++ app/Services/CADECO/Contratos/SubcontratoService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EMartinez
+ * Date: 25/02/2019
+ * Time: 06:58 PM
+ */
+
+namespace App\Services\CADECO\Contratos;
+
+
+class Subcontrato
+{
+
+}
