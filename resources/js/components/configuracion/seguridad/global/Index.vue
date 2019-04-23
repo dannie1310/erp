1cb26dd064dda10c791b4bc96b81d7ec111a50bd
Francisco Esquivel <jfesquivel@grupohi.mx>
2019-04-23 11:48:08 -0500

    Agregar roles nuevos a los componentes de asignación y desasignación

--- resources/js/components/configuracion/seguridad/global/Index.vue
+++ resources/js/components/configuracion/seguridad/global/Index.vue
@@ -1,8 +1,8 @@
 <template>
     <span id="configuracion-seguridad">
-        <asignacion-roles-masiva v-if="$root.can('asignar_roles')"></asignacion-roles-masiva>
-        <desasignacion-roles-masiva v-if="$root.can('asignar_roles')"></desasignacion-roles-masiva>
-        <seguridad-rol-create v-if="$root.can('registrar_roles')"></seguridad-rol-create>
+        <asignacion-roles-masiva ref="asignacion" v-if="$root.can('asignar_roles')"></asignacion-roles-masiva>
+        <desasignacion-roles-masiva ref="desasignacion" v-if="$root.can('asignar_roles')"></desasignacion-roles-masiva>
+        <seguridad-rol-create @creado="addRol($event)" v-if="$root.can('registrar_roles')"></seguridad-rol-create>
         <asignacion-permisos-global v-if="$root.can('asignar_permisos')"></asignacion-permisos-global>
         <rol-delete v-if="$root.can('eliminar_roles')"></rol-delete>
     </span>
@@ -18,6 +18,14 @@
         name: "configuracion-seguridad",
         components: {
             RolDelete,
-            SeguridadRolCreate, AsignacionPermisosGlobal, DesasignacionRolesMasiva, AsignacionRolesMasiva}
+            SeguridadRolCreate, AsignacionPermisosGlobal, DesasignacionRolesMasiva, AsignacionRolesMasiva
+        },
+
+        methods: {
+            addRol(data) {
+                this.$refs.asignacion.addRol(data);
+                this.$refs.desasignacion.addRol(data);
+            }
+        }
     }
 </script>
