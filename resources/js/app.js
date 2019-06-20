5d169425cac9a6d338a4dff21d4f85d457fcc26f
jfesquivel <jfesquivel@grupohi.mx>
2019-06-20 16:31:02 -0500

    Manejo de permisos generales sin contexto en forntend

--- resources/js/app.js
+++ resources/js/app.js
@@ -39,9 +39,8 @@ const app = new Vue({
         MainApp
     },
     methods: {
-        can(permiso) {
-            let permisos = this.$store.getters['auth/permisos'];
-
+        can(permiso, general) {
+            let permisos = general ? this.$store.getters['auth/permisos'] : this.$store.getters['auth/permisosGenerales'];
             if (permisos) {
                 if (Array.isArray(permiso)) {
                     let result = false;
