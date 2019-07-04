81f2aa2f374ceaaefd7857144017e95fa63ab5f0
gsus <jlopeza@grupohi.mx>
2019-07-03 19:52:54 -0500

    bugfix/ERP-479 acotar a 16 digitos la cuenta cargo layout manual

--- app/LAYOUT/DistribucionRecursoRemesaManual.php
+++ app/LAYOUT/DistribucionRecursoRemesaManual.php
@@ -38,7 +38,7 @@ class DistribucionRecursoRemesaManual
             $documento = "D" . str_pad($partida->id_documento, 9, 0, STR_PAD_LEFT);
             $concepto = strlen($partida->documento->Concepto) > 120 ? substr($partida->cuentaAbono->empresa->razon_social, 0, 120):
                 str_pad($partida->documento->Concepto, 120, ' ', STR_PAD_RIGHT);
-            $this->data[] = str_pad($partida->cuentaCargo->numero, 16, ' ', STR_PAD_RIGHT)
+            $this->data[] = str_pad(substr($partida->cuentaCargo->numero, 0, 16), 16, ' ', STR_PAD_RIGHT)
                 . str_pad($partida->cuentaAbono->cuenta_clabe, 20, ' ', STR_PAD_RIGHT)
                 . $partida->cuentaAbono->complemento->nombre_corto
                 . $razon_social
