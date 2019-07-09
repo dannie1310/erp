5bb9af7fcb66dc4a1a3840c1aeea0cf4d96b84f6
Francisco Esquivel <jfesquivel@grupohi.mx>
2019-07-09 11:31:07 -0500

    Cambiar 'asset' por 'mix' al importar los scripts y estilos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,10 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet"/>
 </head>
 <div id="app">
     <main-app/>
 </div>
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 </html>
