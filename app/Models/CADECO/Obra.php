8b1a444a0bb0814449fb9bd03a337c552d97c9de
agarrido <agarrido@grupohi.mx>
2019-07-31 20:42:50 -0500

    Se crean los modelos de unificacionProyectoObra y BaseDatosObra para actualizar los campos EstaAcivo, VisibleEnReportes, VisibleEnApps de Proyectos.Poryectos del moduloSAO

--- app/Models/CADECO/Obra.php
+++ app/Models/CADECO/Obra.php
@@ -75,14 +75,6 @@ class Obra extends Model
         }
     }
 
-    protected static function boot()
-    {
-        parent::boot();
-        self::addGlobalScope(function ($query) {
-            return $query->where('tipo_obra', '!=', 2);
-        });
-    }
-
     public function getAdministradorAttribute()
     {
         if (Context::isEstablished()) {
