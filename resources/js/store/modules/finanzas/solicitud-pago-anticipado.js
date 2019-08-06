5ede31c8598f95f2252dd57dd52d19291f8ffc33
DBenitezC <dbenitezc@grupohi.mx>
2019-08-06 11:31:17 -0500

    Se corrigio la carga del index despues de crear, la validacion quitando los menores a 0.9 y se bloqueo el boton de registrar

--- resources/js/store/modules/finanzas/solicitud-pago-anticipado.js
+++ resources/js/store/modules/finanzas/solicitud-pago-anticipado.js
@@ -30,7 +30,10 @@ export default {
                 }
                 return solicitud
             })
-            state.currentSolicitud != null ? data : null;
+            state.currentSolicitud = data;
+        },
+        UPDATE_ATTRIBUTE(state, data) {
+            state.currentSolicitud[data.attribute] = data.value
         }
     },
 
