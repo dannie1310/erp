a175b9414346d95bb977aabaf5e6c37129012e47
gsus <jlopeza@grupohi.mx>
2019-08-12 20:23:59 -0500

    Perfilamiento de frontend

--- resources/js/components/finanzas/banco/Edit.vue
+++ resources/js/components/finanzas/banco/Edit.vue
@@ -12,7 +12,7 @@
                                           <nav>
                       <div class="nav nav-tabs" id="nav-tab" role="tablist">
                     <!--    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Identificación</a>-->
-                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Cuentas</a>
+                        <a class="nav-item nav-link" v-if="$root.can('consultar_cuenta_corriente')" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Cuentas</a>
                         <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Sucursales</a>
                       </div>
                     </nav>
