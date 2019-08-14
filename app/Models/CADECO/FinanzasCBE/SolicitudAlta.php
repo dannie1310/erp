6a93450ff2d62c07fcc911855a83cac4f499f450
agarrido <agarrido@grupohi.mx>
2019-08-13 20:42:58 -0500

    se incluye Trasaccion

--- app/Models/CADECO/FinanzasCBE/SolicitudAlta.php
+++ app/Models/CADECO/FinanzasCBE/SolicitudAlta.php
@@ -11,6 +11,7 @@ namespace App\Models\CADECO\FinanzasCBE;
 
 use App\Models\CADECO\Finanzas\CuentaBancariaEmpresa;
 use function foo\func;
+use Illuminate\Support\Facades\DB;
 
 class SolicitudAlta extends Solicitud
 {
@@ -77,28 +78,30 @@ class SolicitudAlta extends Solicitud
     }
 
     public function autorizar(){
-        $cuenta = CuentaBancariaEmpresa::query()->create([
-            'id_empresa' => $this->id_empresa,
-            'id_banco' =>  $this->id_banco,
-            'cuenta_clabe' =>  $this->cuenta_clabe,
-            'sucursal' => $this->sucursal,
-            'tipo_cuenta' => $this->tipo_cuenta,
-            'id_solicitud_origen_alta' => $this->id,
-            'id_plaza' => $this->id_plaza,
-            'id_moneda' => $this->id_moneda
-        ]);
+        DB::connection('cadeco')->transaction(function() {
+            $cuenta = CuentaBancariaEmpresa::query()->create( [
+                'id_empresa' => $this->id_empresa,
+                'id_banco' => $this->id_banco,
+                'cuenta_clabe' => $this->cuenta_clabe,
+                'sucursal' => $this->sucursal,
+                'tipo_cuenta' => $this->tipo_cuenta,
+                'id_solicitud_origen_alta' => $this->id,
+                'id_plaza' => $this->id_plaza,
+                'id_moneda' => $this->id_moneda
+            ] );
 
-        $movimiento  = SolicitudMovimiento::query()->where('id_solicitud','=',$this->id)->first();
-        $id = $movimiento->id;
-        $movs  = SolicitudMovimiento::query()->create([
-            'id_solicitud' => $this->id,
-            'id_movimiento_antecedente' =>  $id,
-            'id_tipo_movimiento' =>  2,
-            'observaciones' =>  $this->observaciones,
-        ]);
-        $this->update([
-            'estado' => 2
-        ]);
+            $movimiento = SolicitudMovimiento::query()->where( 'id_solicitud', '=', $this->id )->first();
+            $id = $movimiento->id;
+            $movs = SolicitudMovimiento::query()->create( [
+                'id_solicitud' => $this->id,
+                'id_movimiento_antecedente' => $id,
+                'id_tipo_movimiento' => 2,
+                'observaciones' => $this->observaciones,
+            ] );
+            $this->update( [
+                'estado' => 2
+            ] );
+        });
         return $this;
     }
 }
