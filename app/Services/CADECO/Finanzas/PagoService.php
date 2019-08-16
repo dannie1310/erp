b27b931d17157acaad37fa8a3c90e04c3e7c579d
lmvalencia <luismvalenp@gmail.com>
2019-08-15 20:07:48 -0500

    Filtros para pagos

--- app/Services/CADECO/Finanzas/PagoService.php
+++ app/Services/CADECO/Finanzas/PagoService.php
@@ -9,7 +9,10 @@
 namespace App\Services\CADECO\Finanzas;
 
 
+use App\Models\CADECO\Empresa;
+use App\Models\CADECO\Moneda;
 use App\Models\CADECO\Pago;
+use App\Models\CADECO\Cuenta;
 use App\Repositories\Repository;
 
 class PagoService
@@ -31,7 +34,43 @@ class PagoService
     }
 
     public function paginate($data){
-        return $this->repository->paginate();
+        $pagos = $this->repository;
+
+        if(isset($data['numero_folio'])){
+            $pagos = $pagos->where([['numero_folio','LIKE', '%'.$data['numero_folio'].'%']]);
+        }
+
+        if(isset($data['id_empresa'])){
+            $empresa = Empresa::query()->where([['razon_social', 'LIKE', '%'.$data['id_empresa'].'%']])->get();
+
+                   foreach ($empresa as $e){
+                       $pagos= $pagos->where([['id_empresa', '=', $e->id_empresa]]);
+                   }
+
+        }
+
+        if(isset($data['id_cuenta'])){
+            $cuenta = Cuenta::query()->where([['id_cuenta', 'LIKE', '%'.$data['id_cuenta'].'%']])->get();
+
+            foreach ($cuenta as $e){
+                $pagos= $pagos->where([['id_cuenta', '=', $e->id_cuenta]]);
+            }
+        }
+
+        if(isset($data['observaciones'])){
+            $pagos = $pagos->where([['observaciones','LIKE', '%'.$data['observaciones'].'%']]);
+        }
+
+        if(isset($data['id_moneda'])){
+            $moneda = Moneda::query()->where([['nombre', 'LIKE', '%'.$data['id_moneda'].'%']])->get();
+
+            foreach ($moneda as $e){
+                $pagos= $pagos->where([['id_moneda', '=', $e->id_moneda]]);
+            }
+        }
+
+
+        return $pagos->paginate($data);
     }
 
 }
