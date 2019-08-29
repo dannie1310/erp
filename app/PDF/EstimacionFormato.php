482956aed5342018263e5e431b327191f745b889
lmvalencia <luismvalenp@gmail.com>
2019-08-28 22:46:25 -0500

    PDF ESTIMACION SUBCONTRATO

--- app/PDF/EstimacionFormato.php
+++ app/PDF/EstimacionFormato.php
@@ -42,8 +42,9 @@ class EstimacionFormato extends Rotation
          $this->obra = Obra::find(Context::getIdObra());
          $this->id=$id;
          $this->encabezado_pdf = utf8_decode($this->obra->facturar);
-         $this->estimacion = Estimacion::with('empresa','subcontratoEstimacion','moneda','item','item.concepto', 'item.contrato')->find($id);
+//         $this->estimacion = Estimacion::with('empresa','subcontratoEstimacion','moneda','item','item.concepto', 'item.contrato')->find($id);
 
+        $this->estimacion = Estimacion::query()->find($id);
          $this->contratista =$this->estimacion->empresa->razon_social;
          $this->folio_consecutivo=$this->estimacion->subcontratoEstimacion->NumeroFolioConsecutivo;
 
@@ -241,65 +242,72 @@ class EstimacionFormato extends Rotation
 public function partidas(){
 
 
-//    $this->SetFont('Arial','B',40);
-//    $this->SetTextColor(255,192,203);
-////        $this->SetAlpha(0.5);
-//    $this->RotatedText(35,190, $this->_outerText1, 45);
-//    $this->RotatedText(75,190, $this->_outerText2, 45);
-//    $this->setWaterText("w a t e r M a r k d e m o ", "s e c o n d L i n e o f t e x t");
-//    $this->sin_texto=public_path('pdf/clausulados/SinTexto.jpg');
-//    $img = $this->image(public_path('pdf/clausulados/SinTexto.jpg'), 0, 5, 21);
-//    $this->RotatedImage($img,85,60,40,16,45);
-//    $this->sin_texto=public_path('pdf/clausulados/SinTexto.jpg');
-//    $this->image($this->sin_texto, 0, 5, 21);
-//    $this->RotatedText(100,60,'Hello!',45);
-
-//
-//    $this->Cell(($this->GetPageWidth() - 8) / 8, 0.4,  utf8_decode('CONCILIADOR'), 'TRLB', 0, 'C', 1);
-//    $this->SetAlpha(0.5);
 
 
-//    $watermark = $this->Water(public_path('pdf/clausulados/SinTexto.jpg'));
-//
-//    // Customize some options
-//    $watermark->setFontSize(48)
-//        ->setRotation(30)
-//        ->setOpacity(.4);
-//
-//    // Watermark with Text
-//    $watermark->withText('ajaxray.com', 'path/to/output.jpg');
-//
-//    // Watermark with Image
-//    $watermark->withImage('path/to/logo.png', 'path/to/output.jpg');
-
-        foreach ($this->items as $i => $p) {
+        foreach ($this->estimacion->subcontrato->partidas as $i => $p) {
 
             $this->Ln();
             $this->SetFont('Arial', '', 5);
             $item_antecedente = $p->contrato->id_concepto;
 
-            $this->tran_antecedentes = Item::where('item_antecedente', '=', $item_antecedente)->where("id_transaccion", '<', $this->id)->get()->sum('cantidad');
+//            $this->tran_antecedentes = Item::where('item_antecedente', '=', $item_antecedente)->where("id_transaccion", '<', $this->id)->get()->sum('cantidad');
+            $this->tran_antecedentes = $p->getEstimadoAnteriorAttribute($this->id);
 
+
+            /*Contrato*/
             $this->Cell(0.250 * $this->WidthTotal, 0.3, mb_strtoupper($p->contrato->descripcion), 'RTLB', 0, 'L', 0);   // empty cell with left,top, and right borders
             $this->Cell(0.05 * $this->WidthTotal, 0.3, mb_strtoupper($p->contrato->unidad), 'RTLB', 0, 'C', 0);
             $this->Cell(0.06 * $this->WidthTotal, 0.3, number_format($p->precio_unitario, 3, ".", ","), 'RTLB', 0, 'R', 0);
-            $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($p->contrato->cantidad_original, 3, ".", ","), 'BTLR', 0, 'R', 0);
-            $this->contrato_importe = $p->precio_unitario * $p->contrato->cantidad_original;
+            $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($p->cantidad, 3, ".", ","), 'BTLR', 0, 'R', 0);
+            $this->contrato_importe = $p->precio_unitario * $p->cantidad;
             $this->suma_contrato += $this->contrato_importe;
             $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->contrato_importe, 3, ".", ","), 'BTLR', 0, 'R', 0);
+            /*Estimaciones Anteriores*/
+
             $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->tran_antecedentes, 3, ".", ","), 'BTLR', 0, 'R', 0);
             $this->importe_antecedentes = $this->tran_antecedentes * $p->precio_unitario;
             $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->importe_antecedentes, 3, ".", ","), 'BTLR', 0, 'R', 0);
             $this->suma_estimacionAnterior += $this->importe_antecedentes;
-            $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($p->cantidad, 3, ".", ","), 'BTLR', 0, 'R', 0);
-            $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($p->importe, 3, ".", ","), 'BTLR', 0, 'R', 0);
-            $this->suma_estimacion += $p->importe;
-            $this->cantidad_acumulada = $this->tran_antecedentes + $p->cantidad;
-            $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->cantidad_acumulada, 3, ".", ","), 'BTLR', 0, 'R', 0);
-            $this->importe_acumulado = $this->importe_antecedentes + $p->importe;
-            $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->importe_acumulado, 3, ".", ","), 'BTLR', 0, 'R', 0);
-            $this->suma_acumulada += $this->importe_acumulado;
-            $this->cantidad_restante = $p->contrato->cantidad_original - $this->cantidad_acumulada;
+
+            /*Estimación*/
+
+            $estimacionItem=$p->getEstimacionPartidaAttribute($this->id);
+            $aux=0;
+            if($estimacionItem) {
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($estimacionItem->cantidad, 3, ".", ","), 'BTLR', 0, 'R', 0);
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($estimacionItem->importe, 3, ".", ","), 'BTLR', 0, 'R', 0);
+                $this->suma_estimacion += $estimacionItem->importe;
+            }else{
+
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($aux, 3, ".", ","), 'BTLR', 0, 'R', 0);
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($aux, 3, ".", ","), 'BTLR', 0, 'R', 0);
+            }
+
+
+            /*Acumulada*/
+            if($estimacionItem) {
+                $this->cantidad_acumulada = $this->tran_antecedentes + $estimacionItem->cantidad;
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->cantidad_acumulada, 3, ".", ","), 'BTLR', 0, 'R', 0);
+                $this->importe_acumulado = $this->importe_antecedentes + $estimacionItem->importe;
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->importe_acumulado, 3, ".", ","), 'BTLR', 0, 'R', 0);
+                $this->suma_acumulada += $this->importe_acumulado;
+                $this->cantidad_restante = $estimacionItem->cantidad - $this->cantidad_acumulada;
+
+
+            }else{
+                $this->cantidad_acumulada = $this->tran_antecedentes + $aux;
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->cantidad_acumulada, 3, ".", ","), 'BTLR', 0, 'R', 0);
+                $this->importe_acumulado = $this->importe_antecedentes + $p->importe;
+                $this->Cell((0.130 * $this->WidthTotal) / 2, 0.3, number_format($this->importe_acumulado, 3, ".", ","), 'BTLR', 0, 'R', 0);
+                $this->suma_acumulada += $this->importe_acumulado;
+                $this->cantidad_restante = $p->cantidad - $this->cantidad_acumulada;
+
+
+            }
+
+
+
+
             $this->Cell((0.128 * $this->WidthTotal) / 2, 0.3, number_format($this->cantidad_restante, 3, ".", ","), 'BTLR', 0, 'R', 0);
             $this->importe_restante = $p->precio_unitario * $this->cantidad_restante;
             $this->Cell((0.128 * $this->WidthTotal) / 2, 0.3, number_format($this->importe_restante, 3, ".", ","), 'BTLR', 0, 'R', 0);
