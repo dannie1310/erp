8fb095aa34da1ee37f6f8eef85d2c39e33ee62ee
agarrido <agarrido@grupohi.mx>
2019-09-12 10:45:37 -0500

    S cambia el nombre del permiso de inventario fisico

--- resources/js/components/almacenes/inventario-fisico/Create.vue
+++ resources/js/components/almacenes/inventario-fisico/Create.vue
@@ -1,6 +1,6 @@
 <template>
     <span>
-        <button @click="store" v-if="$root.can('iniciar_conteo_inventario_fisico')" class="btn btn-app btn-info pull-right" :disabled="cargando">
+        <button @click="store" v-if="$root.can('iniciar_inventario_fisico')" class="btn btn-app btn-info pull-right" :disabled="cargando">
             <i class="fa fa-spin fa-spinner" v-if="cargando"></i>
             <i class="fa fa-plus" v-else></i>
             Iniciar Inventario Físico
