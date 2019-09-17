de3cf616d33c5fb0ffe508f8287e8808af28b307
DBenitezC <dbenitezc@grupohi.mx>
2019-09-17 12:02:10 -0500

    inicio del show

---
+++ resources/js/components/almacenes/ajuste-inventario/ajuste-positivo/partials/ActionButtons.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="btn-group">
+        <button @click="show" v-if="value.show" type="button" class="btn btn-sm btn-outline-secondary" title="Ver"><i class="fa fa-eye"></i></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ActionButtons",
+        props: ['value'],
+    }
+</script>
+
+<style scoped>
+
+</style>
