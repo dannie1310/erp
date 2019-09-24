f38e6f5091ab498b30f2d653cbc93330cc077781
JorgeArmando <jamarin@grupohi.mx>
2019-09-24 14:07:35 -0500

    Alta modal REGISTRAR FAMILIA

---
+++ resources/js/components/compras/catalogos/familia/Create.vue
@@ -0,0 +1,98 @@
+<template>
+    <span>
+        <button @click="init" v-if="$root.can('registrar_solicitud_pago_anticipado')" class="btn btn-app btn-info pull-right" :disabled="cargando">
+            <i class="fa fa-spin fa-spinner" v-if="cargando"></i>
+            <i class="fa fa-plus" v-else></i>
+            Registrar Familia
+        </button>
+        <div class="modal fade" ref="modal" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle"> <i class="fa fa-th"></i> REGISTRAR FAMILIA</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form role="form" @submit.prevent="validate">
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group row error-content">
+                                        <label for="tipo" class="col-sm-2 col-form-label">Tipo: </label>
+                                        <div class="col-sm-10">
+                                            <select
+                                                type="text"
+                                                name="tipo"
+                                                data-vv-as="Tipo"
+                                                v-validate="{required: true}"
+                                                class="form-control"
+                                                id="tipo"
+                                                v-model="tipo"
+                                                :class="{'is-invalid': errors.has('tipo')}"
+                                            >
+                                                    <option value>-- Seleccione un tipo --</option>
+                                                    <option v-for="tipo in tipos" :value="tipo.id">{{ tipo.descripcion }}</option>
+                                            </select>
+                                            <div class="invalid-feedback" v-show="errors.has('tipo')">{{ errors.first('tipo') }}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group row error-content">
+                                        <label for="descripcion" class="col-sm-2 col-form-label">Descripción:</label>
+                                        <div class="col-sm-10">
+                                            <input
+                                                :disabled="!tipo"
+                                                type="text"
+                                                name="descripcion"
+                                                data-vv-as="Descripcion"
+                                                v-validate="{required: true}"
+                                                class="form-control"
+                                                id="descripcion"
+                                                placeholder="Descripcion"
+                                                v-model="descripcion"
+                                                :class="{'is-invalid': errors.has('descripcion')}">
+                                            <div class="invalid-feedback" v-show="errors.has('descripcion')">{{ errors.first('descripcion') }}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </span>
+</template>
+
+<script>
+    export default {
+        name: "familia-create",
+        data() {
+            return {
+                cargando:false,
+                tipos: [
+                    {id: 1, descripcion: 'Materiales'},
+                    {id: 2, descripcion: 'Mano de Obra y Servicios'},
+                    {id: 4, descripcion: 'Herramienta y Equipo'},
+                    {id: 8, descripcion: 'Maquinaria'}
+                ],
+                tipo:'',
+                descripcion:''
+            }
+        },
+        methods: {
+            init() {
+                this.cargando = true;
+                $(this.$refs.modal).modal('show');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/compras/catalogos/familia/Index.vue
+++ resources/js/components/compras/catalogos/familia/Index.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="row">
         <div class="col-12">
+            <create @created="paginate()"></create>
+        </div>
+        <div class="col-12">
             <div class="card">
                 <!-- /.card-header -->
                 <div class="card-body">
@@ -17,8 +20,10 @@
 </template>
 
 <script>
+    import Create from "./Create";
     export default {
         name: "familia-index",
+        components:{Create},
         data() {
             return {
                 HeaderSettings: false,
