a14e40c070a38b58da76209e516be3eacc23a325
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2019-10-31 20:51:01 -0600

    Corrección de falta de ortografía en mensaje numero->número

--- app/Models/CADECO/Material.php
+++ app/Models/CADECO/Material.php
@@ -156,7 +156,7 @@ class Material extends Model
     {
         if($this->where('numero_parte','=', $this->numero_parte)->get()->toArray() != [])
         {
-            throw New \Exception('El articulo con el numero de parte "'.$this->numero_parte.'" ya existe.');
+            throw New \Exception('El articulo con el número de parte:"'.$this->numero_parte.'" ya existe.');
         }
     }
 
