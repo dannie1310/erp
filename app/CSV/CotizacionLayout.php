e9830c8c45ed8fa60ab72c703fdd9b80e65642f8
agarrido <agarrido@grupohi.mx>
2019-11-04 14:14:02 -0600

    Se avanza en los datos del layout

--- app/CSV/CotizacionLayout.php
+++ app/CSV/CotizacionLayout.php
@@ -3,10 +3,14 @@
 namespace App\CSV;
 
 use App\Models\CADECO\CotizacionCompra;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Classes\LaravelExcelWorksheet;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Facades\Excel;
 
-class CotizacionLayout implements FromCollection, WithHeadings
+class CotizacionLayout implements WithHeadings, ShouldAutoSize, WithEvents
 {
     protected $cotizacion;
 
@@ -15,21 +19,25 @@ class CotizacionLayout implements FromCollection, WithHeadings
         $this->cotizacion = $cotizacion;
     }
 
-    public function collection()
+    /**
+     * @return array
+     */
+    public function registerEvents(): array
     {
-        $user = array();
-        foreach ($this->cotizacion->cotizaciones as $cot){
-            $folio = str_pad($cot->folio,6,0,0);
-            $user[]=array(
-                "folio"=>$this->cotizacion->folio_format. ' '.chunk_split($folio, 3, ' '),
-                "id"=>$cot->id_transaccion,
-                "panda"=>$this->cotizacion->folio_format. ' '.chunk_split($folio, 3, ' '),
-                "pandachou"=>$cot->id_transaccion
-            );
-
-        }
-
-        return collect($user);
+        return [
+            AfterSheet::class    => function(AfterSheet $event) {
+                $cellRange = 'A1:L2'; // All headers
+                $event->sheet->getDelegate()->getStyle($cellRange)->applyFromArray([
+                    'font' => [
+                        'bold' => true
+                    ]]);
+                $event->sheet->getProtection()->setSheet(true);
+                $event->sheet->protectCells('G3:H3', $this->cotizacion->numero_folio);
+                $event->sheet->protectCells('G4', $this->cotizacion->numero_folio);
+                $event->sheet->protectCells('G14:G20', $this->cotizacion->numero_folio);
+                $event->sheet->protectCells('L3', $this->cotizacion->numero_folio);
+            },
+        ];
     }
 
     public function getFile(){
@@ -37,24 +45,28 @@ class CotizacionLayout implements FromCollection, WithHeadings
         foreach ($this->cotizacion->cotizaciones as $cot){
             $folio = str_pad($cot->folio,6,0,0);
             $user[]=array(
-                "folio"=>$this->cotizacion->folio_format. ' '.chunk_split($folio, 3, ' '),
-                "id"=>$cot->id_transaccion,
+                $this->cotizacion->folio_format. ' '.chunk_split($folio, 3, ' '),
+                $cot->id_transaccion,
             );
 
         }
 
-        return collect($user);
-    }
-
-    public function panda(){
-        return $this->headerDinamicos;
+        return $user;
     }
 
     public function headings(): array
     {
+//        LaravelExcelWorksheet
         return array([' ',' ',' ',' ',' ',' ',$this->cotizacion->empresa->razon_social],
         ['#','DESCRIPCION','IDENTIFICADOR','UNIDAD','CANTIDAD_SOLICITADA','CANTIDAD_APROBADA','Precio Unitario','% Descuento','Precio Total','Moneda',
-            'Precion Total Moneda Conversión','Observaciones']);
+            'Precion Total Moneda Conversión','Observaciones'],
+         ['1','1','1','1','1','1','1','1','=G3*E3-((G3*H3*H3)/100)',$this->getFile()],
+        [' ',' ',' ',' ',' ','%Descuento'],[' ',' ',' ',' ',' ','Subtotal Precios Peso (MXP)'],[' ',' ',' ',' ',' ','%Subtotal Precios Dolar (USD)'],[' ',' ',' ',' ',' ','Subtotal Precios EURO'],[' ',' ',' ',' ',' ','TC USD'],
+        [' ',' ',' ',' ',' ','TC EURO'],[' ',' ',' ',' ',' ','Moneda de Conv.'],[' ',' ',' ',' ',' ','%Subtotal Moneda Conv.'],[' ',' ',' ',' ',' ','IVA'],[' ',' ',' ',' ',' ','Total'],
+        [' ',' ',' ',' ',' ','Fecha de Cotizacion',date("d/m/Y")],[' ',' ',' ',' ',' ','Pago en Parcialdades (%)'],[' ',' ',' ',' ',' ','% Anticipo'],[' ',' ',' ',' ',' ','Credito (dias)'],[' ',' ',' ',' ',' ','Tiempo de Entraga (dias)'],
+        [' ',' ',' ',' ',' ','Vigencia (dias)'],[' ',' ',' ',' ',' ','Observaciones Generales']
+
+        );
 //        [$this->panda()['Precio Unitario'],$this->panda()['% Descuento']],[$this->getFile()]);
     }
 }
