f6724fd61f6c54f2878de8ac8e3104b16fdb0dee
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2019-11-19 20:45:25 -0600

    se complementa backend para calcular el monto pagado correctamente en función del anticipo pagado de manera similar a como lo genera el sp_entrada_material en la línea 74

--- app/Models/CADECO/OrdenCompraPartida.php
+++ app/Models/CADECO/OrdenCompraPartida.php
@@ -33,6 +33,15 @@ class OrdenCompraPartida extends Item
         return $this->hasMany(EntradaMaterialPartida::class, 'item_antecedente', 'id_item');
     }
 
+    public  function facturasPartida(){
+        return $this->hasMany(FacturaPartida::class,  'item_antecedente', 'id_item');
+    }
+
+    public function material()
+    {
+        return $this->belongsTo(Material::class,  'id_material');
+    }
+
     public function scopeDisponibleEntradaAlmacen($query)
     {
         return $query->whereHas('entrega', function ($qu) {
@@ -40,4 +49,34 @@ class OrdenCompraPartida extends Item
         });
     }
 
+    public function getPagadoAttribute()
+    {
+        $pagado = 0;
+        if($this->facturasPartida)
+        {
+            $pagado = round($this->facturasPartida->sum("importe")-$this->facturasPartida->sum("saldo"),2);
+        }
+        return $pagado;
+    }
+    public function getAplicadoAttribute()
+    {
+        $aplicado = round(($this->importe*$this->anticipo/100)-$this->saldo,2);
+        return $aplicado;
+    }
+    public function getPorAplicarAttribute()
+    {
+        return round($this->pagado - $this->aplicado,2);
+    }
+    public function disminuyeSaldo($monto)
+    {
+        $saldo = $this->saldo-$monto;
+        /*Se realiza esta validación por el error existente en el registro de partida de ordenes de compra con anticipo
+        cuyo saldo queda en 0*/
+        if($saldo<-0.01)
+        {
+            $saldo = 0;
+        }
+        $this->saldo = $saldo;
+        $this->save();
+    }
 }
