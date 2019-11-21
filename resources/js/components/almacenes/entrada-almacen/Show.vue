07551239eb82990817c6de15148e60729a70c00a
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2019-11-20 21:22:20 -0600

    Corrección de ícono y texto de la ventana para consulta de entradas de almacén

--- resources/js/components/almacenes/entrada-almacen/Show.vue
+++ resources/js/components/almacenes/entrada-almacen/Show.vue
@@ -7,7 +7,7 @@
             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle"> <i class="fa fa-trash"></i>ENTRADA A ALMACÉN</h5>
+                        <h5 class="modal-title" id="exampleModalLongTitle"> <i class="fa fa-sign-in"></i> ENTRADA DE ALMACÉN</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
