5d835150f4fe04fa945164d8fa143987560dd913
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2019-11-21 22:12:42 -0600

    Agregar transacciones relacionadas y folio de solicitud en componentes de consulta y eliminación

--- resources/js/components/almacenes/entrada-almacen/Delete.vue
+++ resources/js/components/almacenes/entrada-almacen/Delete.vue
@@ -19,7 +19,7 @@
                                     <div class="invoice p-3 mb-3">
                                         <div class="row">
                                             <div class="col-12">
-                                                <h5>Datos de la Entrada Almacén</h5>
+                                                <h5>Datos de la Entrada de Almacén</h5>
                                             </div>
                                         </div>
                                         <div class="row">
@@ -27,22 +27,24 @@
                                                 <table class="table table-striped">
                                                     <tbody>
                                                         <tr>
-                                                            <td class="bg-gray-light"><b>Folio:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.numero_folio_format}}</td>
                                                             <td class="bg-gray-light"><b>Empresa:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.empresa_razon_social}}</td>
+                                                            <td class="bg-gray-light" colspan="5">{{entrada.empresa_razon_social}}</td>
                                                         </tr>
                                                         <tr>
-                                                            <td class="bg-gray-light"><b>Orden de Compra:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.orden_compra_numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"><b>Folio:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"><b>Fecha:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.fecha_format}}</td>
                                                             <td class="bg-gray-light"><b>Referencia:</b></td>
                                                             <td class="bg-gray-light">{{entrada.referencia}}</td>
                                                         </tr>
                                                         <tr>
-                                                            <td class="bg-gray-light"><b>Fecha:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.fecha_format}}</td>
-                                                            <td class="bg-gray-light"><b>Estado:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.estado_format}}</td>
+                                                            <td class="bg-gray-light"><b>Orden de Compra:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.orden_compra_numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"><b>Solicitud:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.solicitud_numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"></td>
+                                                            <td class="bg-gray-light"></td>
                                                         </tr>
                                                     </tbody>
                                                 </table>
@@ -105,11 +107,58 @@
                                                <h6>{{entrada.observaciones}}</h6>
                                             </div>
                                         </div>
+
+                                    </div>
+                                </div>
+                                <div  v-if="entrada.transacciones_relacionadas"  class="card">
+                                    <div class="card-header">
                                         <div class="row">
                                             <div class="col-md-12">
+                                                <label ><i class="fas fa-clone" style="padding-right:3px"></i>Transacciones Relacionadas:</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-12 table-responsive">
+                                                <table class="table table-striped">
+                                                    <thead>
+                                                        <tr>
+                                                            <th class="bg-gray-light index_corto">#</th>
+                                                            <th class="bg-gray-light fecha_hora">Tipo</th>
+                                                            <th class="bg-gray-light fecha">Folio</th>
+                                                            <th class="bg-gray-light fecha">Fecha</th>
+                                                            <th class="bg-gray-light fecha_hora">Fecha/Hora Registro</th>
+                                                            <th class="bg-gray-light">Concepto</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr v-for="(transaccion, i) in entrada.transacciones_relacionadas">
+                                                            <td >{{i+1}}</td>
+                                                            <td >{{transaccion.tipo_transaccion}}</td>
+                                                            <td >{{transaccion.numero_folio}}</td>
+                                                            <td >{{transaccion.fecha}}</td>
+                                                            <td >{{transaccion.fecha_hora_registro}}</td>
+                                                            <td >{{transaccion.concepto}}</td>
+                                                        </tr>
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="invoice p-3 mb-3" style="width:100%">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label for="motivo">Motivo de eliminación:</label>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                            <div class="col-md-12">
                                                 <div class="form-group row error-content">
-                                                     <label for="motivo" class="col-sm-2 col-form-label">Motivo:</label>
-                                                    <div class="col-sm-10">
+
+                                                    <div class="col-md-12">
                                                         <textarea
                                                                 name="motivo"
                                                                 id="motivo"
@@ -123,7 +172,6 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
--- resources/js/components/almacenes/entrada-almacen/Show.vue
+++ resources/js/components/almacenes/entrada-almacen/Show.vue
@@ -27,22 +27,24 @@
                                                 <table class="table table-striped">
                                                     <tbody>
                                                         <tr>
-                                                            <td class="bg-gray-light"><b>Folio:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.numero_folio_format}}</td>
                                                             <td class="bg-gray-light"><b>Empresa:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.empresa_razon_social}}</td>
+                                                            <td class="bg-gray-light" colspan="5">{{entrada.empresa_razon_social}}</td>
                                                         </tr>
                                                         <tr>
-                                                            <td class="bg-gray-light"><b>Orden de Compra:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.orden_compra_numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"><b>Folio:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"><b>Fecha:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.fecha_format}}</td>
                                                             <td class="bg-gray-light"><b>Referencia:</b></td>
                                                             <td class="bg-gray-light">{{entrada.referencia}}</td>
                                                         </tr>
                                                         <tr>
-                                                            <td class="bg-gray-light"><b>Fecha:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.fecha_format}}</td>
-                                                            <td class="bg-gray-light"><b>Estado:</b></td>
-                                                            <td class="bg-gray-light">{{entrada.estado_format}}</td>
+                                                            <td class="bg-gray-light"><b>Orden de Compra:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.orden_compra_numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"><b>Solicitud:</b></td>
+                                                            <td class="bg-gray-light">{{entrada.solicitud_numero_folio_format}}</td>
+                                                            <td class="bg-gray-light"></td>
+                                                            <td class="bg-gray-light"></td>
                                                         </tr>
                                                     </tbody>
                                                 </table>
@@ -107,7 +109,44 @@
                                         </div>
                                     </div>
                                 </div>
+                                <div  v-if="entrada.transacciones_relacionadas"  class="card">
+                                    <div class="card-header">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <label ><i class="fas fa-clone" style="padding-right:3px"></i>Transacciones Relacionadas:</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-12 table-responsive">
+                                                <table class="table table-striped">
+                                                    <thead>
+                                                        <tr>
+                                                            <th class="bg-gray-light index_corto">#</th>
+                                                            <th class="bg-gray-light fecha_hora">Tipo</th>
+                                                            <th class="bg-gray-light fecha">Folio</th>
+                                                            <th class="bg-gray-light fecha">Fecha</th>
+                                                            <th class="bg-gray-light fecha_hora">Fecha/Hora Registro</th>
+                                                            <th class="bg-gray-light">Concepto</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr v-for="(transaccion, i) in entrada.transacciones_relacionadas">
+                                                            <td >{{i+1}}</td>
+                                                            <td >{{transaccion.tipo_transaccion}}</td>
+                                                            <td >{{transaccion.numero_folio}}</td>
+                                                            <td >{{transaccion.fecha}}</td>
+                                                            <td >{{transaccion.fecha_hora_registro}}</td>
+                                                            <td >{{transaccion.concepto}}</td>
+                                                        </tr>
 
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="modal-footer">
@@ -135,7 +174,7 @@
                 this.$store.commit('almacenes/entrada-almacen/SET_ENTRADA', null);
                 return this.$store.dispatch('almacenes/entrada-almacen/find', {
                     id: this.id,
-                    params: { include: ['partidas','partidas.contratista'] }
+                    params: { include: ['partidas','partidas.contratista','transacciones_relacionadas'] }
                 }).then(data => {
                     this.$store.commit('almacenes/entrada-almacen/SET_ENTRADA', data);
                     this.partidas = this.entrada.partidas.data;
