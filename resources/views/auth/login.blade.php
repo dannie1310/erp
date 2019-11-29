f3742bee3a721b7421fda1a89ed1041d9a03d543
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2019-11-29 09:33:54 -0600

    Se agrega al login blade el kit gratuito de FA y la referencia a mix(app.css) y mix(app.js)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet"/>
     <style>
         #content {
             width: 100%; height: 100%;
@@ -16,6 +16,9 @@
             position: absolute; top: 0; left: 0;
         }
     </style>
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="https://kit.fontawesome.com/4a7d805650.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
 </head>
 <body>
 <div id="content">
@@ -33,7 +36,7 @@
                     <div class="input-group mb-3">
                         <input type="text" name="usuario" class="form-control{{ $errors->has('usuario') ? ' is-invalid' : '' }}" placeholder="Usuario" value="{{ old('usuario') }}" required autofocus>
                         <div class="input-group-append">
-                            <span class="fa fa-user input-group-text"></span>
+                            <span class="fas fa-user input-group-text"></span>
                         </div>
                         @if ($errors->has('usuario'))
                             <span class="invalid-feedback" role="alert">
@@ -62,6 +65,5 @@
         </div>
     </div>
 </div>
-<script src="{{ asset('js/login.js') }}"></script>
 </body>
 </html>
