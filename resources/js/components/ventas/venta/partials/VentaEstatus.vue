0aa47445bc5d8eea2c39a23decf3a30e8728aede
JorgeArmando <jamarin@grupohi.mx>
2019-12-18 16:52:44 -0600

    Correccion de conflictos Y merge

--- resources/js/components/ventas/venta/partials/VentaEstatus.vue
+++ resources/js/components/ventas/venta/partials/VentaEstatus.vue
@@ -1,8 +1,8 @@
 <template>
     <small class="badge" :class="{
-                    'badge-danger': value.estado == 'CANCELADO',
-                    'badge-primary': value.estado == 'CREADO',
-                    'badge-success': value.estado == 'PAGADO'}">
+                     'badge-danger': value.estado == 'CANCELADO',
+                     'badge-primary': value.estado == 'CREADO',
+                     'badge-success': value.estado == 'PAGADO'}">
         {{ value.estado }} </small>
 </template>
 
