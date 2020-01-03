935ef1720e9d4ecac41525723560685f621eb740
DBenitezC <dbenitezc@grupohi.mx>
2020-01-03 13:18:30 -0600

    Inicio del Index de Clientes

---
+++ resources/js/components/catalogos/empresa/cliente/Index.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="row">
+        <!--<div class="col-12">-->
+        <!--<create @created="paginate()"></create>-->
+        <!--</div>-->
+        <div class="col-12">
+            <div class="card">
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <datatable v-bind="$data" />
+                    </div>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+        <!-- /.col -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "familia-maq-index",
+        // components:{Create},
+        data() {
+            return {
+                HeaderSettings: false,
+                columns: [
+                    { title: '#', field: 'index', sortable: false },
+                    { title: 'RFC', field: 'rfc', sortable: true, thComp: require('../../../globals/th-Filter')},
+                    { title: 'Razon Social', field: 'razon_social', sortable: true, thComp: require('../../../globals/th-Filter')},
+                    { title: 'Tipo Cliente', field: 'tipo_cliente', sortable: true, thComp: require('../../../globals/th-Filter')},
+                    { title: 'Acciones', field: 'buttons',  tdComp: require('./partials/ActionButtons').default}
+                ],
+                data: [],
+                total: 0,
+                query: {scope:'tipo:8',  sort: 'razon_social', order: 'desc'},
+                estado: "",
+                cargando: false
+            }
+        },
+        mounted() {
+            this.$Progress.start();
+            this.paginate()
+                .finally(() => {
+                    this.$Progress.finish();
+                })
+        },
+
+        methods: {
+            paginate() {
+                this.cargando = true;
+                return this.$store.dispatch('cadeco/familia/paginate', { params: this.query})
+                    .then(data => {
+                        this.$store.commit('cadeco/familia/SET_FAMILIAS', data.data);
+                        this.$store.commit('cadeco/familia/SET_META', data.meta);
+                    })
+                    .finally(() => {
+                        this.cargando = false;
+                    })
+            },
+        },
+        computed: {
+            familias(){
+                return this.$store.getters['cadeco/familia/familias'];
+            },
+            meta(){
+                return this.$store.getters['cadeco/familia/meta'];
+            },
+            tbodyStyle() {
+                return this.cargando ?  { '-webkit-filter': 'blur(2px)' } : {}
+            }
+        },
+        watch: {
+            familias: {
+                handler(famls) {
+                    let self = this
+                    self.$data.data = []
+                    self.$data.data = famls.map((familia, i) => ({
+                        index: (i + 1) + self.query.offset,
+                        descripcion: familia.descripcion
+                    }));
+                },
+                deep: true
+            },
+
+            meta: {
+                handler(meta) {
+                    let total = meta.pagination.total
+                    this.$data.total = total
+                },
+                deep: true
+            },
+            query: {
+                handler(query) {
+                    this.paginate(query)
+                },
+                deep: true
+            },
+            search(val) {
+                if (this.timer) {
+                    clearTimeout(this.timer);
+                    this.timer = null;
+                }
+                this.timer = setTimeout(() => {
+                    this.query.search = val;
+                    this.query.offset = 0;
+                    this.paginate();
+
+                }, 500);
+            },
+            cargando(val) {
+                $('tbody').css({
+                    '-webkit-filter': val ? 'blur(2px)' : '',
+                    'pointer-events': val ? 'none' : ''
+                });
+            }
+        }
+    }
+</script>
+<style>
+    .money
+    {
+        text-align: right;
+    }
+</style>
