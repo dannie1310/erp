f42b57d921d61e56ed99fa60e5569972bf5583d4
JorgeArmando <jamarin@grupohi.mx>
2020-01-03 19:42:12 -0600

    Se realiza el fron para catalogo de EFO´s

--- resources/js/components/control-interno/partials/Menu.vue
+++ resources/js/components/control-interno/partials/Menu.vue
@@ -25,6 +25,23 @@
                     </li>
                 </ul>
             </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link" @click="mostrarMenu($event)">
+                    <i class="nav-icon fa fa-university"></i>
+                    <p>
+                        Finanzas
+                        <i class="right fa fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item" >
+                        <router-link :to="{name: 'familia'}" class="nav-link" :class="{active: this.$route.name == 'familia'}">
+                            <i class="fa fa-times nav-icon"></i>
+                            <p>EFO´s</p>
+                        </router-link>
+                    </li>
+                </ul>
+            </li>
         </ul>
     </nav>
 </template>
