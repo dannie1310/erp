e68101b89dcb3e5595ca09eb60d2c1b34b3ab6b6
DBenitezC <dbenitezc@grupohi.mx>
2020-01-07 14:02:58 -0600

    Se agrega tipo de cliente y porcentaje de participacion, y validaciones

--- resources/js/components/catalogos/empresa/cliente/Create.vue
+++ resources/js/components/catalogos/empresa/cliente/Create.vue
@@ -19,18 +19,18 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group row error-content">
-                                        <label for="descripcion" class="col-sm-2 col-form-label">Razón Social:</label>
+                                        <label for="razon_social" class="col-sm-2 col-form-label">Razón Social:</label>
                                         <div class="col-sm-10">
                                             <input
                                                     style="text-transform:uppercase;"
                                                     type="text"
-                                                    name="Razón Social"
-                                                    data-vv-as="razon_social"
+                                                    name="razon_social"
+                                                    data-vv-as="Razón Social"
                                                     v-validate="{required: true}"
                                                     class="form-control"
                                                     id="razon_social"
                                                     placeholder="Razón Social"
-                                                    v-model="razon_social"
+                                                    v-model="registro_cliente.razon_social"
                                                     :class="{'is-invalid': errors.has('razon_social')}">
                                             <div class="invalid-feedback" v-show="errors.has('razon_social')">{{ errors.first('razon_social') }}</div>
                                         </div>
@@ -43,9 +43,10 @@
                                         <label for="rfc" class="col-md-2" ><b>RFC: </b> </label>
                                         <div class="col-md-10">
                                             <input class="form-control"
+                                                   style="text-transform:uppercase;"
                                                    name="rfc"
                                                    data-vv-as="RFC"
-                                                   v-model="rfc"
+                                                   v-model="registro_cliente.rfc"
                                                    :class="{'is-invalid':rfcValidate}"
                                                    v-validate="{ required: true, regex: /\.(js|ts)$/ }"
                                                    id="rfc"
@@ -55,11 +56,51 @@
                                     </div>
                                 </div>
                             </div>
-
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group row error-content">
+                                        <label for="id_tipo_cliente" class="col  sm-2 col-form-label">Tipo Cliente: </label>
+                                        <div class="col-sm-10">
+                                            <div class="btn-group btn-group-toggle">
+                                                <label class="btn btn-outline-secondary" :class="registro_cliente.id_tipo_cliente === Number(key) ? 'active': ''" v-for="(tipo_cliente, key) in tipos_clientes" :key="key">
+                                                    <input type="radio"
+                                                        class="btn-group-toggle"
+                                                        name="id_tipo_cliente"
+                                                        :id="'tipo_cliente' + key"
+                                                        :value="key"
+                                                        autocomplete="on"
+                                                        v-model.number="registro_cliente.id_tipo_cliente">
+                                                        {{ tipo_cliente }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group row error-content">
+                                        <label for="porcentaje" class="col-sm-2 col-form-label">Porcentaje de Participación</label>
+                                        <div class="col-sm-10">
+                                            <input
+                                                    type="number"
+                                                    name="porcentaje"
+                                                    data-vv-as="Porcentaje de Participación"
+                                                    v-validate="{required: true, numeric: true, min_value:0.1, max_value: 100}"
+                                                    class="form-control"
+                                                    id="porcentaje"
+                                                    placeholder="Porcentaje de Participación"
+                                                    v-model="registro_cliente.porcentaje"
+                                                    :class="{'is-invalid': errors.has('porcentaje')}">
+                                            <div class="invalid-feedback" v-show="errors.has('porcentaje')">{{ errors.first('porcentaje') }}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                                <button type="submit" class="btn btn-primary":disabled="errors.count() > 0 ">Registrar</button>
+                                <button type="submit" class="btn btn-primary" :disabled="errors.count() > 0 || registro_cliente.id_tipo_cliente == ''">Registrar</button>
                         </div>
                     </form>
                 </div>
@@ -75,9 +116,17 @@
         data() {
             return {
                 cargando : false,
-                razon_social : '',
-                rfc : '',
-                tipo_cliente : '',
+                registro_cliente : {
+                    id_tipo_cliente : '',
+                    razon_social : '',
+                    rfc : '',
+                    porcentaje : 0,
+                },
+                tipos_clientes: {
+                    1: "Comprador",
+                    2: "Inversionista",
+                    3: "Comprador e Inversionista"
+                },
                 rfcValidate: false
             }
         },
@@ -85,9 +134,6 @@
             init() {
                 this.cargando = false;
                 $(this.$refs.modal).modal('show');
-                this.razon_social = '';
-                this.rfc = '';
-                this.tipo_cliente = '';
             },
             store() {
                 return this.$store.dispatch('cadeco/cliente/store', this.$data)
@@ -100,12 +146,12 @@
             },
             validate() {
                 this.$validator.validate().then(result => {
-                    if(!rfcRegex.test(this.banco.rfc)){
+                    if(!rfcRegex.test(this.registro_cliente.rfc)){
+                        console.log(this.registro_cliente)
                         return this.invalidRFC();
                     } else{
                         this.rfcValidate=false;
                     }
-
                     if (result) {
                         this.store()
                     }
@@ -119,5 +165,10 @@
 </script>
 
 <style scoped>
-
+    .error-label {
+        width: 100%;
+        margin-top: 0.25rem;
+        font-size: 80%;
+        color: #dc3545;
+    }
 </style>
