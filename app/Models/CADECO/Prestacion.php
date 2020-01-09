bcd007a1f5c6f57b5a1c8363a8781b6304df56cb
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2020-01-08 18:48:56 -0600

    Terminación de lógica de actualización de control de obra en el pago de listas de raya y prestaciones

---
+++ app/Models/CADECO/Prestacion.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EMartinez
+ * Date: 08/01/2020
+ * Time: 05:09 PM
+ */
+
+namespace App\Models\CADECO;
+
+
+class Prestacion extends Transaccion
+{
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::addGlobalScope(function ($query) {
+            return $query->where('tipo_transaccion', '=', 102)
+                ->where('opciones', '=', 0);
+        });
+    }
+
+    public function items()
+    {
+        return $this->hasMany(ItemPrestacion::class, 'id_transaccion', 'id_transaccion');
+    }
+
+    /**
+     * Este método implementa la lógica actualización de control de obra del procedimiento almacenado sp_aplicar_pagos
+     * y se detona al registrar una orden de pago relacionada a una factura que ampara una prestación
+     */
+    public function actualizaControlObra(ItemFactura $item_factura, OrdenPago $orden_pago)
+    {
+        $importe = round($orden_pago->monto * -1 * $item_factura->proporcion_item, 2);
+        $factor = $importe / $this->monto;
+        if ($this->items) {
+            foreach ($this->items as $item) {
+                $pago = round(($item->inventario->monto_pagado + $item->importe * $factor), 2);
+                $item->inventario->monto_pagado = $pago;
+                $item->inventario->save();
+                $item->inventario->distribuirPagoInventarios();
+            }
+        }
+    }
+}
