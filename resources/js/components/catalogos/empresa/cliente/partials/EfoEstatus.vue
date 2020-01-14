f7a0de919e400235857e107b5da2e72cdc5904bb
DBenitezC <dbenitezc@grupohi.mx>
2020-01-14 17:56:54 -0600

    Se filtra por estado de efo

--- resources/js/components/catalogos/empresa/cliente/partials/EfoEstatus.vue
+++ resources/js/components/catalogos/empresa/cliente/partials/EfoEstatus.vue
@@ -1,6 +1,6 @@
 <template>
-    <small v-if="value.estado.id == 2 || value.estado.id == 0" class="badge" :class="{'badge-warning': value.estado.id == 2, 'badge-danger' : value.estado.id == 0 }">
-        {{value.estado.descripcion}}
+    <small v-if="value.id == 2 || value.id == 0" class="badge" :class="{'badge-warning': value.id == 2, 'badge-danger' : value.id == 0 }">
+        {{value.descripcion}}
     </small>
 
 </template>
