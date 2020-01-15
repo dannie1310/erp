712290bbd1fd2b4e3f3f7e19fbfea6030df0d04e
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2020-01-15 10:47:33 -0600

    Se concluye formato de contrarecibo

--- app/PDF/Finanzas/ContrareciboPDF.php
+++ app/PDF/Finanzas/ContrareciboPDF.php
@@ -33,26 +33,92 @@ class ContrareciboPDF extends Rotation
     }
     function Header()
     {
+        $this->SetFont('Arial', 'B', 12);
+        $this->Cell(13.5);
+        $this->Cell(3.5, .7, 'FOLIO ', 'LT', 0, 'L');
+        $this->Cell(2.5, .7, $this->factura->contra_recibo->numero_folio_format . ' ', 'RT', 0, 'L');
+        $this->Ln(.7);
+        $this->SetFont('Arial', 'B', 25);
+        $this->Cell(13.5, .7, 'CONTRARECIBO', 0, 0, 'C', 0);
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(3.5, .7, 'FECHA ', 'LB', 0, 'L');
+        $this->Cell(2.5, .7, $this->factura->contra_recibo->fecha_format . ' ', 'RB', 0, 'L');
+        $this->Ln(1);
+
+
+        $this->SetFont('Arial', 'B', 13);
+        $this->Cell(19.5, 0.7, $this->factura->obra->descripcion . ' ', 1, 0, 'C');
+        $this->Ln(1);
+
+        $this->SetFont('Arial', '', 10);
+        $this->Cell(19.5, .5, 'Recibimos de:', 0, 0, 'L');
+        $this->Ln(0.5);
+        $this->MultiCell(19.5, .5, $this->factura->empresa->razon_social , 1, 'J',  0);
+        $this->Ln(0.5);
+
+        $this->Cell(19.5, .5, utf8_decode('La siguiente factura para su revisión: '), 0, 'L');
+        $this->Ln(0.5);
 
 
     }
+    function factura(){
+
+        $this->SetFont('Arial', '', 9);
+        $this->SetFillColor(180, 180, 180);
+        $this->SetWidths(array(0.60,  4.45, 3.15, 3.15, 5, 3.15));
+        $this->SetStyles(array('DF', 'DF', 'DF', 'FD', 'DF'));
+        $this->SetRounds(array('1', '', '', '', '', '2'));
+        $this->SetRadius(array(0.2, 0, 0,  0, 0, 0.2));
+        $this->SetFills(array('180,180,180', '180,180,180', '180,180,180', '180,180,180', '180,180,180', '180,180,180'));
+        $this->SetTextColors(array('0,0,0', '0,0,0', '0,0,0', '0,0,0', '0,0,0', '0,0,0'));
+        $this->SetHeights(array(0.5));
+        $this->SetAligns(array('C', 'C', 'C', 'C', 'C', 'C'));
+        $this->Row(array("#",  utf8_decode("Número"), "Fecha", "Vencimiento", "Importe", "Moneda"));
+
+        $this->SetWidths(array(0.60, 4.45, 3.15, 3.15, 5, 3.15));
+        $this->SetRounds(array('4', '', '', '', '', '3'));
+        $this->SetRadius(array(0.2, 0, 0, 0, 0, 0.2));
+        $this->SetFills(array('255,255,255', '255,255,255', '255,255,255', '255,255,255', '255,255,255', '255,255,255'));
+        $this->SetAligns(array('C', 'L', 'C', 'C', 'R', 'C'));
+        $this->SetTextColors(array('0,0,0', '0,0,0', '0,0,0', '0,0,0', '0,0,0', '0,0,0'));
+
+        $this->Row(array(1, $this->factura->referencia, $this->factura->fecha_format, $this->factura->vencimiento_format, $this->factura->monto_format, $this->factura->moneda->nombre));
+
+        $this->Ln(.5);
+        $this->SetFont('Arial', '', 10);
+        $this->CellFitScale(19.5, .5, 'Observaciones:', 0, 0, 'L');
+        $this->Ln(.5);
+        $this->MultiCell(19.5, .5, $this->factura->contra_recibo->observaciones . ' ', 1, 'J',  0);
+        $this->Ln(.5);
+        $this->SetFont('Arial', 'B', 15);
+        $this->CellFitScale(19.5, .5, utf8_decode('Los presente factura se toma a revisión SIN originar obligación alguna para pago'), 0, 0, 'L');
+    }
+
+    function firmas(){
+        $this->SetY(-3.8);
+        $this->Cell(12.5);
+        $this->SetTextColor('0,0,0');
+        $this->SetFont('Arial', '', 10);
+        $this->CellFitScale(7, .5, ('Recibi '), 1, 0, 'C');
+        $this->Ln(.5);
+        $this->Cell(12.5);
+        $this->CellFitScale(7, 2, ' ', 1, 0, 'R');
+    }
 
     function Footer(){
-        /*$this->firmas();*/
+        $this->firmas();
         $this->SetY(-3.8);
-
-
-
-
-
         $this->SetY(-1);
         $this->SetTextColor('0,0,0');
         $this->SetFont('Arial', 'BI', 6);
-        $this->Cell(10, .4, utf8_decode('Formato generado desde el sistema de finanzas. Fecha de registro: '.$this->factura->fecha_hora_registro_format), 0, 0, 'L');
+        $this->Cell(15, .4, utf8_decode('Formato generado desde el sistema de finanzas. Fecha de registro: '
+            .$this->factura->fecha_hora_registro_format)
+            .' Fecha de consulta: '.date("d-m-Y h:i:s")
+            , 0, 0, 'L');
 
         $this->SetFont('Arial', 'B', 6);
         $this->SetTextColor('100,100,100');
-        $this->Cell(9.5, .4, utf8_decode('Sistema de Administración de Obra'), 0, 0, 'R');
+        $this->Cell(4.5, .4, utf8_decode('Sistema de Administración de Obra'), 0, 0, 'R');
 
         $this->SetY(-0.7);
         $this->SetTextColor('0,0,0');
@@ -65,6 +131,7 @@ class ContrareciboPDF extends Rotation
         $this->AliasNbPages();
         $this->AddPage();
         $this->SetAutoPageBreak(true,3.75);
+        $this->factura();
 
 
         try {
