2e6c90abcb2d16415765bed2fe4ac6ee09692341
DBenitezC <dbenitezc@grupohi.mx>
2020-01-17 20:35:56 -0600

    Se cambia la validacion para duplicidad de rfc o razon social

--- app/Models/CADECO/Destajista.php
+++ app/Models/CADECO/Destajista.php
@@ -24,19 +24,18 @@ class Destajista extends Empresa
         });
     }
 
-    public function validaDuplicidadRfc()
+    public function validaDuplicidadRfc($nuevos_datos)
     {
-        $destajista = Destajista::whereRaw("(razon_social = '".$this->razon_social."' or rfc ='".$this->rfc."' )")->orderBy('id_empresa', 'desc')->first();
 
-        if(!is_null($destajista))
-        {
-            if(is_null($this->id_empresa) || ($this->id_empresa != $destajista->id_empresa)) { //creación
-                if ($destajista->rfc === $this->rfc) {
-                    throw New \Exception('Este rfc se encuentra registrado previamente.');
-                }
-                if ($destajista->razon_social === $this->razon_social) {
-                    throw New \Exception('Esta razón social se encuentra registrada previamente.');
-                }
+        $destajista = Destajista::whereRaw("(razon_social = '".$nuevos_datos->razon_social."' or rfc ='".$nuevos_datos->rfc."' )")
+            ->where('id_empresa', '!=', $nuevos_datos->id_empresa)->first();
+
+        if(!is_null($destajista)) {
+            if ($destajista->rfc === $nuevos_datos->rfc) {
+                throw New \Exception('Este rfc se encuentra registrado previamente.');
+            }
+            if ($destajista->razon_social === $nuevos_datos->razon_social) {
+                throw New \Exception('Esta razón social se encuentra registrada previamente.');
             }
         }
     }
