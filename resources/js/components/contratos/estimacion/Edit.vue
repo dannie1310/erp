d1b8b9b7c87c69f145fb67792917e6811acf25f0
DBenitezC <dbenitezc@grupohi.mx>
2020-02-10 12:02:16 -0600

    Se inicia edicion del frontend de edicion de estimaciones

--- resources/js/components/contratos/estimacion/Edit.vue
+++ resources/js/components/contratos/estimacion/Edit.vue
@@ -1,187 +1,103 @@
 <template>
-     <span>
-       <div class="d-flex flex-row-reverse">
-                                    <div class="p-2">
-                                        <button @click="editar"  type="button" :disabled="cargando" class="btn btn-info" ><i class="fa fa-pencil"></i> &nbsp; Editar Estimación</button>
-                                    </div>
-                                </div>
-        <div class="row">  
-            <div class="col-12 mb-5" v-if="!cargando">
-                <div class="card">
-                    <div class="card-body" v-if="estimacion">
-                            <div class="row mt-5 mb-5">
-                              <div class="col-8">
-                                <div class="row">
-                                      <div class="col-4">
-                                     
-                                      </div>
-                                <div class="col-8 text-center mt-5">
-                                      <h1>{{ obra.facturar }}</h1>
-                                      <h6 class="mt-5">Cuerpo de Estimación</h6>
-                                </div>
-                               </div>
-                              </div>
-                              <div class="col-4">
-                                <table class="table table-bordered table-hover">
-                                   <thead>
-                                      <tr>
-                                        <th scope="col">Folio SAO</th>
-                                        <th class="text-right" scope="col">#{{ estimacion.estimacion.numero_folio.padStart(5,"0") }}</th>
-                                      </tr>
-                                   </thead>
-                                   <tbody>
-                                        <tr>
-                                       <th scope="row">No. Estimación</th>
-                                       <td class="text-right" v-if="estimacion.numEstimacion"> {{ estimacion.numEstimacion.NumeroFolioConsecutivo }}</td>
-                                       <td class="text-right" v-else></td>
-                                       </tr>
-                                        <tr>
-                                       <th scope="row">Semana de Contrato</th>
-                                       <td></td>
-                                       </tr>
-                                        <tr>
-                                       <th scope="row">Fecha</th>
-                                       <td class="text-right"> {{ estimacion.fecha }}</td>
-                                       </tr>
-                                        <tr>
-                                       <th scope="row">Periodo</th>
-                                       <td class="text-right">De : {{ estimacion.fecha_inicial }} A: {{ estimacion.fecha_final }}</td>
-                                       </tr>
-                                   </tbody>
-                               </table>
-                              </div>
-                             </div>
+    <span>
+        <div class="row">
+			<div class="col-md-6">
+				<div class="card" v-if="estimacion">
+					<div class="card-header">
+						<h6 class="card-title">Subcontrato</h6>
+					</div>
+					<div class="card-body">
+						<form>
+							<div class="form-group row">
+								<label for="fecha" class="col-sm-2 col-form-label">Fecha</label>
+								<div class="col-sm-10">
+									<input
+                                        name="fecha"
+                                        v-validate="{required: true}"
+                                        data-vv-as="Fecha"
+                                        :class="{'is-invalid': errors.has('fecha')}"
+                                        v-model="fecha"
+                                        type="date"
+                                        class="form-control"
+                                        id="fecha"
+                                        placeholder="Fecha">
+									<div class="invalid-feedback" v-show="errors.has('fecha')">{{ errors.first('fecha') }}</div>
+								</div>
+							</div>
+							<div class="form-group row">
+								<label class="col-sm-2 col-form-label">Objeto</label>
+								<div class="col-sm-10">
+									{{ subcontrato.referencia }}
+								</div>
+							</div>
+							<div class="form-group row" v-if="subcontrato.empresa">
+								<label class="col-sm-2 col-form-label">Contratista</label>
+								<div class="col-sm-10">
+									{{ subcontrato.empresa.razon_social }}
+								</div>
+							</div>
+							<div class="form-group row">
+								<label class="col-sm-2 col-form-label">Observaciones</label>
+								<div class="col-sm-10">
+									<textarea
+                                        name="observaciones"
+                                        id="observaciones"
+                                        class="form-control"
+                                        v-model="observaciones"
+                                    ></textarea>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 
-                        <table class="table table-hover table-bordered" v-if="estimacion">
-                          <thead>
-                            <tr>
-                              <th>Organización:</th>
-                              <th class="text-center" scope="row">{{ obra.nombre }}</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <th>Contratista: </th>
-                              <th class="text-center" scope="row">{{ estimacion.empresa.razon_social }}</th>
-                            </tr>
-                            <tr>
-                              <th>No. de Contrato: </th>
-                              <th class="text-center" scope="row">{{ estimacion.subcontrato.referencia }}</th>
-                            </tr>
-                          </tbody>
-                        </table>
-                        <!-- Partidas--->
+			<div class="col-md-6">
+				<div class="card" v-if="subcontrato">
+					<div class="card-header">
+						<h6 class="card-title">Periodo de Estimación</h6>
+					</div>
+					<div class="card-body">
+						<form>
+							<div class="form-row">
+								<div class="form-group col-md-6">
+									<label for="inicio">Inicio</label>
+									<input
+                                        name="fecha_inicio"
+                                        v-validate="{required: true}"
+                                        data-vv-as="Inicio"
+                                        :class="{'is-invalid': errors.has('fecha_inicio')}"
+                                        v-model="fecha_inicio"
+                                        type="date"
+                                        class="form-control"
+                                        id="fecha_inicio"
+                                        placeholder="Inicio">
+									<div class="invalid-feedback" v-show="errors.has('fecha_inicio')">{{ errors.first('fecha_inicio') }}</div>
 
-                    <div class="col-12 mt-5" >
-                       <table class="table table-hover table-bordered">
-                            <thead class="thead-dark text-center">
-                                <tr>
-                                    <th rowspan="2">Concepto</th>
-                                    <th rowspan="2">U.M.</th>
-                                    <th rowspan="2">P.U.</th>
-                                    <th colspan="2">Contrato y Aditamentos</th>
-                                    <th colspan="2">Acum. A Estimación Anterior</th>
-                                    <th colspan="2">Esta Estimación </th>
-                                    <th colspan="2">Acum. A Esta Estimación </th>
-                                    <th colspan="2">Saldo por Estimar</th>
-                                </tr>
-                                <tr>
-                                    <th scope="col">Cantidad</th>
-                                    <th scope="col"> Importe</th>
-                                    <th scope="col">Cantidad</th>
-                                    <th scope="col"> Importe</th>
-                                    <th scope="col">Cantidad</th>
-                                    <th scope="col"> Importe</th>
-                                    <th scope="col">Cantidad</th>
-                                    <th scope="col"> Importe</th>
-                                    <th scope="col">Cantidad</th>
-                                    <th scope="col"> Importe</th>
-                                </tr>
-                            </thead>
+								</div>
+								<div class="form-group col-md-6">
+									<label for="inicio">Término</label>
+									<input
+                                        name="fecha_fin"
+                                        v-validate="{required: true}"
+                                        data-vv-as="Término"
+                                        :class="{'is-invalid': errors.has('fecha_fin')}"
+                                        v-model="fecha_fin"
+                                        type="date"
+                                        class="form-control"
+                                        id="fecha_fin"
+                                        placeholder="Término">
+									<div class="invalid-feedback" v-show="errors.has('fecha_fin')">{{ errors.first('fecha_fin') }}</div>
 
-                            <tbody>
-                                <tr>
-                                    <th class="text-secondary">OBRA EJECUTADA</th>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td class="text-center">{{ estimacion.moneda.nombre }}</td>
-                                      <td></td>
-                                    <td class="text-center">{{ estimacion.moneda.nombre }}</td>
-                                      <td></td>
-                                    <td class="text-center">{{ estimacion.moneda.nombre }}</td>
-                                     <td></td>
-                                    <td class="text-center">{{ estimacion.moneda.nombre }}</td>
-                                      <td></td>
-                                    <td class="text-center">{{ estimacion.moneda.nombre }}</td>
-                                </tr>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 
-                                <template  v-for="(item,index) in estimacion.items">
-
-                                   <tr v-if="!isObject(item)">
-                                    <th>{{ identacionTabla(index) }} {{ item }}</th>
-                                    <td class="text-center"></td>
-                                    <td class="text-right"> </td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                    <td class="text-right"></td>
-                                </tr>
-
-                                <tr v-else>
-
-                                    <td>{{ identacionItem(index.length/4) }} {{ item.concepto }}</td>
-                                    <td class="text-center">{{ item.unidad }}</td>
-                                    <td class="text-right">{{ parseFloat(item.precioUnitario).formatMoney(4,'.',',') }} </td>
-                                    <td class="text-right">{{  parseFloat(item.cantidadContrato).formatMoney(4,'.',',') }}</td>
-                                    <td class="text-right">{{  parseFloat(item.importeContrato).formatMoney(4,'.',',') }}</td>
-                                    <td class="text-right">{{  parseFloat(item.cantidadEstimadoAnterior).formatMoney(4,'.',',')  }}</td>
-                                    <td class="text-right">{{  parseFloat(item.importeEstimadoAnterior).formatMoney(4,'.',',') }}</td>
-                                    <td class="text-right">{{  parseFloat(item.cantidadEstimacion).formatMoney(4,'.',',') }}</td>
-                                    <td class="text-right">{{  parseFloat(item.importeEstimacion).formatMoney(4,'.',',')  }}</td>
-                                    <td class="text-right">{{  parseFloat(item.cantidadAcumulado).formatMoney(4,'.',',') }}</td>
-                                    <td class="text-right">{{  parseFloat(item.importeAcumulado).formatMoney(4,'.',',') }}</td>
-                                    <td class="text-right">{{  parseFloat(item.cantidadPorEstimar).formatMoney(4,'.',',') }} </td>
-                                    <td class="text-right">{{  parseFloat(item.importePorEstimar).formatMoney(4,'.',',')  }}</td>
-                                </tr>
-                          </template>
-                        <!--Sumas totales de la s partidas-->
-
-                                  <tr class="bg-dark">
-                                    <th class="text-right">Subtotales Obra Ejecutada</th>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td class="text-right">{{estimacion.suma_contrato }}</td>
-                                    <td></td>
-                                    <td class="text-right"> {{ estimacion.suma_estimadoAnterior }}</td>
-                                    <td></td>
-                                    <td class="text-right"> {{ estimacion.suma_estimacion }}</td>
-                                    <td></td>
-                                    <td class="text-right"> {{ estimacion.suma_acumulado }}</td>
-                                    <td> </td>
-                                    <td class="text-right">{{ estimacion.suma_porEstimar }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-12" v-if="cargando" style="text-align: center;">
-              <div class="loadingio-spinner-spinner-oegch9ef2xk"><div class="ldio-0bxwyxm0owg">
-                 <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-                 </div>
-            </div>
-        </div>
-     </span>
+    </span>
 </template>
 
 
@@ -217,7 +133,6 @@
                 return this.$store.dispatch('contratos/estimacion/showEstimacionTable', {
                     id: this.id,
                 }).then(data => {
-                  
                     this.$store.commit('contratos/estimacion/SET_ESTIMACION', data);
                     this.cargando = false;
                 })
