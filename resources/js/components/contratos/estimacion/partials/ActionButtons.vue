a3d0e413f9107d556a9f213462345542413a2aa0
gsus <jlopeza@grupohi.mx>
2020-02-13 20:54:21 -0600

    ERP 1406 Correccion resumen estimacion

--- resources/js/components/contratos/estimacion/partials/ActionButtons.vue
+++ resources/js/components/contratos/estimacion/partials/ActionButtons.vue
@@ -55,10 +55,6 @@
                                     <th style="text-align: right">$ {{value.estimacion.total_retenciones}}</th>
                                 </tr>
                                 <tr v-if="configuracion.retenciones_antes_iva == 1">
-                                    <th style="text-align: left" colspan="2">Retención de IVA</th>
-                                    <th style="text-align: right">$ {{value.estimacion.retencion_iva}}</th>
-                                </tr>
-                                <tr v-if="configuracion.retenciones_antes_iva == 1">
                                     <th style="text-align: left" colspan="2">Total Retenciones Liberadas</th>
                                     <th style="text-align: right">$ {{value.estimacion.total_retencion_liberadas}}</th>
                                 </tr>
@@ -73,6 +69,10 @@
                                 <tr>
                                     <th style="text-align: left" colspan="2">I.V.A.</th>
                                     <td style="text-align: right">{{value.estimacion.iva_orden_pago}}</td>
+                                </tr>
+                                <tr v-if="configuracion.retenciones_antes_iva == 1">
+                                    <th style="text-align: left" colspan="2">Retención de IVA</th>
+                                    <th style="text-align: right">{{value.estimacion.retencion_iva_format}}</th>
                                 </tr>
                                 <tr>
                                     <th style="text-align: left" colspan="2">Total</th>
