049695cec5155ef35158e62e2dbc717297eef698
JorgeArmando <jamarin@grupohi.mx>
2020-03-18 18:56:40 -0600

    avance 1

---
+++ resources/js/components/finanzas/insumo-servicio/mano-obra/partials/ActionButtons.vue
@@ -0,0 +1,18 @@
+<template>
+    <span>
+        <div class="btn-group">
+            
+        </div>
+    </span>
+</template>
+
+<script>
+export default {
+    name: "ActionButtons",
+    props: ['value']
+}
+</script>
+
+<style scoped>
+
+</style>
