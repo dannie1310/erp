c77c697352d1491358cd078433d5bef5bd362c23
DBenitezC <dbenitezc@grupohi.mx>
2020-04-08 11:34:29 -0500

    Se inicia el backend para eliminar una solicitud de compra

--- resources/js/components/compras/solicitud-compra/Show.vue
+++ resources/js/components/compras/solicitud-compra/Show.vue
@@ -92,7 +92,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div v>
+                    </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                     </div>
