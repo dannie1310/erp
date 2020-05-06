c5aa714a3cf8cb5d5da7991d7d8154411b99b8d5
JorgeArmando <jamarin@grupohi.mx>
2020-05-06 16:17:04 -0500

    Se crea avance para ver pandita

--- resources/js/components/compras/cotizacion/partials/ActionButtons.vue
+++ resources/js/components/compras/cotizacion/partials/ActionButtons.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="btn-group">
         <Show v-bind:id="value.id" v-bind:show="value.show"></Show>
+        <DescargarLayout v-bind:id="value.id"></DescargarLayout>
     </div>
 </template>
 <script>
 import Show from '../Show';
+import DescargarLayout from '../DescargaLayout';
     export default {
         name: "solicitud-compra-buttons",
-        components: {Show},
+        components: {Show, DescargarLayout},
         props: ['value'],
     }
 </script>
