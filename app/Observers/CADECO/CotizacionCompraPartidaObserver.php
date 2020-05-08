606e64bb6771d5cb6b3bdb6452f6c0bd8e1633ba
DBenitezC <dbenitezc@grupohi.mx>
2020-05-08 10:57:26 -0500

    guardar respaldo de cotizacion partida

--- app/Observers/CADECO/CotizacionCompraPartidaObserver.php
+++ app/Observers/CADECO/CotizacionCompraPartidaObserver.php
@@ -4,7 +4,57 @@
 namespace App\Observers\CADECO;
 
 
+use App\Models\CADECO\Compras\CotizacionPartidaEliminada;
+use App\Models\CADECO\CotizacionCompraPartida;
+
 class CotizacionCompraPartidaObserver
 {
+    public function deleting(CotizacionCompraPartida $partida)
+    {
+        CotizacionPartidaEliminada::create(
+            [
+                'id_transaccion' => $partida->id_transaccion,
+                'id_material' => $partida->,
+                'numero',
+                'disponibles',
+                'cantidad',
+                'precio_unitario',
+                'descuento',
+                'descuento_adicional',
+                'otros_descuentos',
+                'flete',
+                'anticipo',
+                'dias_credito',
+                'dias_entrega',
+                'no_cotizado',
+                'id_moneda',
+                'capacidad',
+                'descuento_partida',
+                'observaciones',
+                'estatus',
+                'timestamp_registro',
+                'usuario_elimina' => auth()->id(),
+                'fecha_eliminacion' => date('Y-m-d H:i:s')
 
+            ]
+        );
+
+        EntregaEliminada::create(
+            [
+                'id_item' => $partida->id_item,
+                'numero_entrega' => $partida->entrega->numero_entrega,
+                'fecha' => $partida->entrega->fecha,
+                'cantidad' => $partida->entrega->cantidad,
+                'surtida' => $partida->entrega->surtida,
+                'pedidas' => $partida->entrega->pedidas,
+                'asignadas' => $partida->entrega->asignadas,
+                'id_concepto' => $partida->entrega->id_concepto,
+                'id_almacen' => $partida->entrega->id_almacen
+            ]
+        );
+        $partida->entrega->delete();
+        if($partida->complemento)
+        {
+            $partida->complemento->delete();
+        }
 }
