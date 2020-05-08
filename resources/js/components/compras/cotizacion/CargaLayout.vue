6dd0a78768f456270e175eacae9f203bdf66ca6c
JorgeArmando <jamarin@grupohi.mx>
2020-05-08 16:30:00 -0500

    Se crea commit para arreglar incidencia

---
+++ resources/js/components/compras/cotizacion/CargaLayout.vue
@@ -0,0 +1,161 @@
+<template>
+    <span>
+        <button @click="find()" type="button" class="btn btn-sm btn-outline-secondary" :disabled="cargando" title="Cargar Layout Cotización">
+            <i class="fa fa-upload" v-if="!cargando"></i>
+            <i class="fa fa-spinner fa-spin" v-else></i>
+        </button>
+        <div class="modal fade" ref="modal" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle"> <i class="fa fa-thumbs-o-up"></i> CARGAR LAYOUT DE COTIZACION</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form role="form" @submit.prevent="validate">
+                        <!-- <div class="modal-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="invoice p-3 mb-3">
+                                        <table class="table">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="bg-gray-light"><b>Folio:</b></td>
+                                                    <td class="bg-gray-light">{{res.numero_folio_format}}</td>
+                                                    <td class="bg-gray-light"><b>Folio Compuesto:</b></td>
+                                                    <td class="bg-gray-light">{{res.compuesto ? res.compuesto.folio : '----'}}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="bg-gray-light"><b>Fecha Requerido:</b></td>
+                                                    <td class="bg-gray-light">{{res.fecha_format}}</td>
+                                                    <td class="bg-gray-light"><b>Usuario Registró:</b></td>
+                                                    <td class="bg-gray-light">{{usuario}}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="bg-gray-light"><b>Fecha / Hora Registro</b></td>
+                                                    <td class="bg-gray-light">{{res.fecha_registro}}</td>
+                                                    <td class="bg-gray-light"><b>Observaciones:</b></td>
+                                                    <td class="bg-gray-light" colspan="3">{{res.observaciones}}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <h6><b>Detalle de las partidas</b></h6>
+                                            </div>
+                                        </div>
+                                <div class="row">
+                                    <div class="table-responsive col-md-12">
+                                            <table class="table table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>Material</th>
+                                                        <th>Unidad</th>
+                                                        <th class="no_parte">Solicitado</th>
+                                                        <th class="no_parte">En O/C(s)</th>
+                                                        <th class="no_parte">Surtido</th>
+                                                        <th class="no_parte">Existencia</th>
+                                                        <th class="money">Autorizado</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr v-for="(partida, i) in partidas">
+                                                        <td>{{i+1}}</td>
+                                                        <td >{{partida.material.descripcion}}</td>
+                                                        <td style="text-align: center">{{partida.unidad}}</td>
+                                                        <td style="text-align: center">{{partida.solicitado_cantidad}}</td>
+                                                        <td style="text-align: center">{{partida.orden_compra_cantidad}}</td>
+                                                        <td style="text-align: center">{{partida.surtido_cantidad}}</td>
+                                                        <td style="text-align: center">{{partida.existencia_cantidad}}</td>
+                                                        <td style="text-align: right">
+                                                            <input
+                                                                :disabled="cargando"
+                                                                type="number"
+                                                                step=".1"
+                                                                :name="`autorizado[${i}]`"
+                                                                data-vv-as="Autorizado"
+                                                                v-validate="{required: true, min_value:0.01, max_value:partida.cantidad}"
+                                                                class="form-control"
+                                                                v-model="cantidad[i]"
+                                                                id="autorizado"
+                                                                :class="{'is-invalid': errors.has(`autorizado[${i}]`)}">
+                                                    <div class="invalid-feedback" v-show="errors.has(`autorizado[${i}]`)">{{ errors.first(`autorizado[${i}]`) }}</div>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                            <button type="submit" class="btn btn-primary" :disabled="errors.count() > 0">Aprobar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </span>
+</template>
+
+<script>
+    export default {
+        name: "carga-layout-cotizacion",
+        props: ['id'],
+        data(){
+            return{
+                cargando: false,
+                no_cotizados: [],
+                items: [],
+                cuenta: [],
+                x: 0,
+                t: 0,
+            }
+        },
+        methods: {
+            find() {
+
+                this.cargando = true;
+                this.$store.commit('compras/cotizacion/SET_COTIZACION', null);
+                return this.$store.dispatch('compras/cotizacion/find', {
+                    id: this.id,
+                    params:{include: ['empresa', 'sucursal', 'complemento', 'cotizaciones.material', 'cotizaciones.moneda']}
+                }).then(data => {
+                    this.$store.commit('compras/cotizacion/SET_COTIZACION', data);
+                    this.items = data.cotizaciones.data;
+                    $(this.$refs.modal).appendTo('body')
+                    $(this.$refs.modal).modal('show')
+                    this.cargando = false;
+                    
+                })
+            }
+        },
+        computed: {
+            cotizacion() {
+                return this.$store.getters['compras/cotizacion/currentCotizacion']
+            }
+        },
+        watch: {
+            items()
+            {
+                this.x = 0;
+                this.t = 0;
+                while(this.x < this.items.length)
+                {
+                    this.no_cotizados[this.x] = this.items[this.x].no_cotizado;
+                    this.cuenta[this.x] = (this.no_cotizados[this.x]) ? this.t ++ : 0;
+                    this.x ++;
+                }                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/compras/cotizacion/partials/ActionButtons.vue
+++ resources/js/components/compras/cotizacion/partials/ActionButtons.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="btn-group">
         <Show v-bind:id="value.id" v-bind:show="value.show"></Show>
+        <CargaLayout v-bind:id="value.id"></CargaLayout>
         <button @click="edit" v-if="$root.can('editar_cotizacion_compra')" type="button" class="btn btn-sm btn-outline-info" title="Editar">
                 <i class="fa fa-pencil"></i>
         </button>
@@ -8,9 +9,10 @@
 </template>
 <script>
 import Show from '../Show';
+import CargaLayout from '../CargaLayout';
     export default {
         name: "cotizacion-buttons",
-        components: {Show},
+        components: {Show, CargaLayout},
         props: ['value'],
 
         methods: {
