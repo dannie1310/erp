3dd2cac1f29967e3f282e735c9a2646f46b5ccb7
JorgeArmando <jamarin@grupohi.mx>
2020-05-08 19:00:14 -0500

    Se continua con desarrollo de carga de lay out
    Se modifica en la vista el modal para cargar el archivo xlxs.
    Se crea rutas para comunicar con el back.

--- resources/js/components/compras/cotizacion/CargaLayout.vue
+++ resources/js/components/compras/cotizacion/CargaLayout.vue
@@ -5,96 +5,38 @@
             <i class="fa fa-spinner fa-spin" v-else></i>
         </button>
         <div class="modal fade" ref="modal" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-dialog modal-dialog-centered modal" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle"> <i class="fa fa-thumbs-o-up"></i> CARGAR LAYOUT DE COTIZACION</h5>
+                        <h5 class="modal-title" id="exampleModalLongTitle"> <i class="fa fa-upload"></i> CARGAR LAYOUT DE COTIZACÓN</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <form role="form" @submit.prevent="validate">
-                        <!-- <div class="modal-body">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="invoice p-3 mb-3">
-                                        <table class="table">
-                                            <tbody>
-                                                <tr>
-                                                    <td class="bg-gray-light"><b>Folio:</b></td>
-                                                    <td class="bg-gray-light">{{res.numero_folio_format}}</td>
-                                                    <td class="bg-gray-light"><b>Folio Compuesto:</b></td>
-                                                    <td class="bg-gray-light">{{res.compuesto ? res.compuesto.folio : '----'}}</td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="bg-gray-light"><b>Fecha Requerido:</b></td>
-                                                    <td class="bg-gray-light">{{res.fecha_format}}</td>
-                                                    <td class="bg-gray-light"><b>Usuario Registró:</b></td>
-                                                    <td class="bg-gray-light">{{usuario}}</td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="bg-gray-light"><b>Fecha / Hora Registro</b></td>
-                                                    <td class="bg-gray-light">{{res.fecha_registro}}</td>
-                                                    <td class="bg-gray-light"><b>Observaciones:</b></td>
-                                                    <td class="bg-gray-light" colspan="3">{{res.observaciones}}</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                        <div class="row">
-                                            <div class="col-12">
-                                                <h6><b>Detalle de las partidas</b></h6>
-                                            </div>
-                                        </div>
-                                <div class="row">
-                                    <div class="table-responsive col-md-12">
-                                            <table class="table table-striped">
-                                                <thead>
-                                                    <tr>
-                                                        <th>#</th>
-                                                        <th>Material</th>
-                                                        <th>Unidad</th>
-                                                        <th class="no_parte">Solicitado</th>
-                                                        <th class="no_parte">En O/C(s)</th>
-                                                        <th class="no_parte">Surtido</th>
-                                                        <th class="no_parte">Existencia</th>
-                                                        <th class="money">Autorizado</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr v-for="(partida, i) in partidas">
-                                                        <td>{{i+1}}</td>
-                                                        <td >{{partida.material.descripcion}}</td>
-                                                        <td style="text-align: center">{{partida.unidad}}</td>
-                                                        <td style="text-align: center">{{partida.solicitado_cantidad}}</td>
-                                                        <td style="text-align: center">{{partida.orden_compra_cantidad}}</td>
-                                                        <td style="text-align: center">{{partida.surtido_cantidad}}</td>
-                                                        <td style="text-align: center">{{partida.existencia_cantidad}}</td>
-                                                        <td style="text-align: right">
-                                                            <input
-                                                                :disabled="cargando"
-                                                                type="number"
-                                                                step=".1"
-                                                                :name="`autorizado[${i}]`"
-                                                                data-vv-as="Autorizado"
-                                                                v-validate="{required: true, min_value:0.01, max_value:partida.cantidad}"
-                                                                class="form-control"
-                                                                v-model="cantidad[i]"
-                                                                id="autorizado"
-                                                                :class="{'is-invalid': errors.has(`autorizado[${i}]`)}">
-                                                    <div class="invalid-feedback" v-show="errors.has(`autorizado[${i}]`)">{{ errors.first(`autorizado[${i}]`) }}</div>
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                  </div>
+
+                        <div class="modal-body">
+                            <div class="row justify-content-between">
+                                <div class="col-md-12">
+                                     <label for="carga_layout" class="col-lg-12 col-form-label">Cargar Layout</label>
+                                    <div class="col-lg-12">
+                                        <input type="file" class="form-control" id="carga_layout"
+                                               @change="onFileChange"
+                                               row="3"
+                                               v-validate="{ ext: ['xlsx']}"
+                                               name="carga_layout"
+                                               data-vv-as="Layout"
+                                               ref="carga_layout"
+                                               :class="{'is-invalid': errors.has('carga_layout')}">
+                                        <div class="invalid-feedback" v-show="errors.has('carga_layout')">{{ errors.first('carga_layout') }} (csv)</div>
                                     </div>
                                 </div>
                             </div>
-                        </div> -->
+                        </div>
+
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                            <button type="submit" class="btn btn-primary" :disabled="errors.count() > 0">Aprobar</button>
+                            <button type="button" class="btn btn-secondary" @click="cerrarModal">Cerrar</button>
+                            <button type="button" class="btn btn-primary" @click="validate" :disabled="!file">Cargar</button>
                         </div>
                     </form>
                 </div>
@@ -115,25 +57,79 @@
                 cuenta: [],
                 x: 0,
                 t: 0,
+                data: null,
+                file: null,
             }
         },
         methods: {
             find() {
 
+                // console.log(this.$refs.carga_layout.value);
                 this.cargando = true;
-                this.$store.commit('compras/cotizacion/SET_COTIZACION', null);
-                return this.$store.dispatch('compras/cotizacion/find', {
-                    id: this.id,
-                    params:{include: ['empresa', 'sucursal', 'complemento', 'cotizaciones.material', 'cotizaciones.moneda']}
-                }).then(data => {
-                    this.$store.commit('compras/cotizacion/SET_COTIZACION', data);
-                    this.items = data.cotizaciones.data;
-                    $(this.$refs.modal).appendTo('body')
-                    $(this.$refs.modal).modal('show')
-                    this.cargando = false;
+                this.$refs.carga_layout.value = '';
+                this.file = null;
+                this.$validator.errors.clear();
+
+                $(this.$refs.modal).appendTo('body')
+                $(this.$refs.modal).modal('show');
+                this.cargando = false;                
+                
+
+                // this.cargando = true;
+                // this.$store.commit('compras/cotizacion/SET_COTIZACION', null);
+                // return this.$store.dispatch('compras/cotizacion/find', {
+                //     id: this.id,
+                //     params:{include: ['empresa', 'sucursal', 'complemento', 'cotizaciones.material', 'cotizaciones.moneda']}
+                // }).then(data => {
+                //     this.$store.commit('compras/cotizacion/SET_COTIZACION', data);
+                //     this.items = data.cotizaciones.data;
+                //     $(this.$refs.modal).appendTo('body')
+                //     $(this.$refs.modal).modal('show')
+                //     this.cargando = false;
                     
-                })
-            }
+                // })
+            },
+            createImage(file, tipo) {
+                var reader = new FileReader();
+                var vm = this;
+
+                reader.onload = (e) => {
+                    vm.file = e.target.result;
+                };
+                reader.readAsDataURL(file);
+
+            },
+            onFileChange(e){
+                this.file = null;
+                var files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                    return;
+                if(e.target.id == 'carga_layout') {
+                    this.createImage(files[0]);
+                }
+            },
+            cerrarModal(event) {
+                this.$refs.carga_layout.value = '';
+                this.file = null;
+                this.$validator.errors.clear();
+                $(this.$refs.modal).modal('hide')
+            },
+            validate() {
+                
+                this.$validator.validate().then(result => {
+                    if (result){
+                        this.cargarLayout()
+                    }else{
+                        if(this.$refs.carga_layout.value !== ''){
+                            this.$refs.carga_layout.value = '';
+                            this.file = null;
+                        }
+                        this.$validator.errors.clear();
+                        swal('¡Error!', 'Error archivos de entrada invalidos.', 'error')
+                    }
+                });
+                
+            },
         },
         computed: {
             cotizacion() {
