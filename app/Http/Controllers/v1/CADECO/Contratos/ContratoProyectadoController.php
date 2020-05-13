d27242f47108c6bb102bfe3fd80970fb7bcf427c
gsus <jlopeza@grupohi.mx>
2020-05-12 20:54:33 -0500

    ERP 1604 Perfilamiento para liberación a pruebas

--- app/Http/Controllers/v1/CADECO/Contratos/ContratoProyectadoController.php
+++ app/Http/Controllers/v1/CADECO/Contratos/ContratoProyectadoController.php
@@ -52,6 +52,8 @@ class ContratoProyectadoController extends Controller
         $this->middleware('context');
         $this->middleware('permiso:modificar_area_subcontratante_cp')->only('actualiza');
         $this->middleware('permiso:consultar_contrato_proyectado')->only(['index','paginate','find','show']);
+        $this->middleware('permiso:registrar_contrato_proyectado')->only(['store']);
+        
         $this->service = $service;
         $this->fractal = $fractal;
         $this->transformer = $transformer;
