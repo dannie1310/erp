9d14a11cb63fa16c4f14a33cd8b4196dbf92ec09
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2020-07-07 23:40:41 -0500

    Se finaliza informe de CFD cargados x Empresa x Mes

--- app/Informes/CFDEmpresaMes.php
+++ app/Informes/CFDEmpresaMes.php
@@ -9,13 +9,79 @@
 namespace App\Informes;
 
 
+use Illuminate\Support\Facades\DB;
+
 class CFDEmpresaMes
 {
 
     public static function get()
     {
+        return CFDEmpresaMes::getArreglo();
 
 
     }
 
+    private static function getArreglo()
+    {
+        $query = "SELECT ListaEmpresasSAT.id,
+       ListaEmpresasSAT.razon_social,
+       ListaEmpresasSAT.rfc,
+       COUNT (cfd_sat.id) AS cantidad,
+       month (cfd_sat.fecha) AS mes,
+       year (cfd_sat.fecha) AS anio,
+       CatalogoMeses.NombreCorto AS mes_txt,
+       RIGHT ('00' + CONVERT (VARCHAR (2), month (fecha)), 2) AS mes_z,
+       '[' + rfc + '] ' + razon_social AS cfd_rs
+  FROM ((SEGURIDAD_ERP.Contabilidad.cfd_sat cfd_sat
+         INNER JOIN
+         (SELECT cfd_sat.id
+            FROM SEGURIDAD_ERP.Contabilidad.cfd_sat cfd_sat,
+                 (SELECT MAX (cfd_sat.fecha) AS fecha_fin,
+                         DATEADD (month, -13, max (cfd_sat.fecha))
+                            AS fecha_inicio
+                    FROM SEGURIDAD_ERP.Contabilidad.cfd_sat cfd_sat) Subquery
+           WHERE cfd_sat.fecha BETWEEN Subquery.fecha_inicio
+                                   AND Subquery.fecha_fin) Subquery
+            ON (cfd_sat.id = Subquery.id))
+        INNER JOIN
+        SEGURIDAD_ERP.Contabilidad.ListaEmpresasSAT ListaEmpresasSAT
+           ON (cfd_sat.id_empresa_sat = ListaEmpresasSAT.id))
+       INNER JOIN SEGURIDAD_ERP.Reportes.CatalogoMeses CatalogoMeses
+          ON (month (cfd_sat.fecha) = CatalogoMeses.MesID)
+GROUP BY ListaEmpresasSAT.razon_social,
+         ListaEmpresasSAT.rfc,
+         month (cfd_sat.fecha),
+         year (cfd_sat.fecha),
+         ListaEmpresasSAT.id,
+         CatalogoMeses.NombreCorto";
+
+        $informe = DB::select($query);
+        $informe = array_map(function ($value) {
+            return (array)$value;
+        }, $informe);
+        $informe = CFDEmpresaMes::reorganizaArreglo($informe);
+        return $informe;
+    }
+
+    private static function reorganizaArreglo($informe)
+    {
+        $empresas=[];
+        $anio_mes=[];
+        $valores= [];
+        foreach ($informe as $item) {
+            $empresas[$item["rfc"]]["valor"] = $item["cfd_rs"];
+            $anio_mes[$item["anio"].$item["mes_z"]] = $item["mes_txt"].' '.$item["anio"];
+            $valores[$item["rfc"]][$item["anio"].$item["mes_z"]] =($item["cantidad"]>0)?number_format($item["cantidad"],0,".",","):"-";
+        }
+        ksort($empresas);
+        krsort($anio_mes);
+        array_pop($anio_mes);
+        ksort($anio_mes);
+        $i = 1;
+        foreach ($empresas as $k=>$v){
+            $empresas[$k]["k"] = $i;
+            $i++;
+        }
+        return ["empresas"=>$empresas, "anios_meses"=>$anio_mes, "valores"=>$valores];
+    }
 }
--- resources/js/components/fiscal/cfd/cfd-sat/InformeCFDEmpresaMes.vue
+++ resources/js/components/fiscal/cfd/cfd-sat/InformeCFDEmpresaMes.vue
@@ -1,5 +1,41 @@
 <template>
-    <span>informe</span>
+    <span>
+        <div class="row" v-if="cargando">
+            <div class="col-md-12">
+                <div class="spinner-border text-success" role="status">
+                   <span class="sr-only">Cargando...</span>
+                </div>
+            </div>
+        </div>
+        <span v-else-if="informe && cargando== false">
+            <div class="row" >
+                <div class="col-md-12">
+                    <table class="table">
+                        <tbody>
+                            <tr style="background-color: #757575; color:#FFF; text-align:center" >
+                                <td class="index_corto">#</td>
+                                <td >Empresa</td>
+                                <template v-for="(anio_mes,i) in informe.anios_meses">
+                                    <td>{{anio_mes}}</td>
+                                </template>
+
+                            </tr>
+                            <template v-for="(empresa,j) in informe.empresas">
+                               <tr >
+                                   <td class="index_corto">{{empresa.k}}</td>
+                                   <td>{{empresa.valor}}</td>
+                                   <template v-for="(anio_mes,i) in informe.anios_meses">
+                                       <td style="text-align: right" v-if="informe.valores[j][i] != null">{{informe.valores[j][i]}}</td>
+                                       <td style="text-align: right" v-else>-</td>
+                                   </template>
+                               </tr>
+                            </template>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </span>
+    </span>
 </template>
 
 <script>
@@ -8,7 +44,6 @@
         data() {
             return {
                 informe : [],
-                fechas : [],
                 cargando : false
             }
         },
@@ -23,7 +58,6 @@
                 })
                     .then(data => {
                         this.informe = data.informe;
-                        this.fechas = data.fechas;
                     })
                     .finally(() => {
                         this.cargando = false;
