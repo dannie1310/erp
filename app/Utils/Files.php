bbffb5b177601bb18a0e3dfdde1e58c995807b4f
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2020-07-08 18:48:18 -0500

    cambiar Files por self dentro de clase Files

--- app/Utils/Files.php
+++ app/Utils/Files.php
@@ -11,43 +11,43 @@ namespace App\Utils;
 
 class Files
 {
-    public static $files;
-    public static $anidacion="";
-    public static $i=0;
-    public static $ifil=1;
+    protected static $files;
+    protected static $anidacion="";
+    protected static $i=0;
+    protected static $ifil=1;
 
     public static function getFiles($path){
-        if(Files::$i > 0){
-            Files::$anidacion .= "____";
+        if(self::$i > 0){
+            self::$anidacion .= "____";
         }
 
         $dir = opendir(public_path($path));
         while ($current = readdir($dir)){
             if( $current != "." && $current != "..") {
                 if(is_dir($path.$current)) {
-                    Files::$files[Files::$i]["anidacion"] = Files::$anidacion;
-                    Files::$files[Files::$i]["nombre"] = $current;
-                    Files::$files[Files::$i]["contador"] = "";
-                    Files::$files[Files::$i]["clase"] = "fa fa-folder-open";
-                    Files::$i +=1;
-                    Files::getFiles($path.$current.'/');
+                    self::$files[self::$i]["anidacion"] = self::$anidacion;
+                    self::$files[self::$i]["nombre"] = $current;
+                    self::$files[self::$i]["contador"] = "";
+                    self::$files[self::$i]["clase"] = "fa fa-folder-open";
+                    self::$i +=1;
+                    self::getFiles($path.$current.'/');
                 }
                 else {
-                    Files::$files[Files::$i]["anidacion"] = Files::$anidacion;
-                    Files::$files[Files::$i]["nombre"] = $current;
-                    Files::$files[Files::$i]["contador"] = Files::$ifil;
+                    self::$files[self::$i]["anidacion"] = self::$anidacion;
+                    self::$files[self::$i]["nombre"] = $current;
+                    self::$files[self::$i]["contador"] = self::$ifil;
                     if (strpos($current,".zip")){
-                        Files::$files[Files::$i]["clase"] = "fa fa-file-zip-o";
+                        self::$files[self::$i]["clase"] = "fa fa-file-zip-o";
                     } else if (strpos($current,".xml")){
-                        Files::$files[Files::$i]["clase"] = "fa fa-file-code";
+                        self::$files[self::$i]["clase"] = "fa fa-file-code";
                     } else {
-                        Files::$files[Files::$i]["clase"] = "fa fa-file";
+                        self::$files[self::$i]["clase"] = "fa fa-file";
                     }
-                    Files::$ifil +=1;
-                    Files::$i +=1;
+                    self::$ifil +=1;
+                    self::$i +=1;
                 }
             }
         }
-        return Files::$files;
+        return self::$files;
     }
 }
