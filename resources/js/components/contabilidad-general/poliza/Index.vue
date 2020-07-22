b79dacd3c47d0771dfef53a64d27f278f500abfa
DBenitezC <dbenitezc@grupohi.mx>
2020-07-22 17:20:59 -0500

    Cambios en el index para las polizas

--- resources/js/components/contabilidad-general/poliza/Index.vue
+++ resources/js/components/contabilidad-general/poliza/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <span>
-        <div class="row">
+     <!--   <div class="row">
             <div class="col-12">
                 <show v-bind:tipo_modal="tipo_modal"></show>
             </div>
@@ -10,159 +10,48 @@
                 <edit v-bind:tipo_modal="tipo_modal" v-bind:id_empresa="id_empresa"></edit>
             </div>
         </div>
-
+-->
         <div class="row">
             <div class="col-md-12">
                 <h6><i class="fa fa-plug" ></i>Datos de Conexión:</h6>
             </div>
         </div>
-         <div class="row">
-             <div class="col-md-6">
-                 <div class="form-group row error-content">
-                     <label for="id_empresa" class="col-md-2 col-form-label">Empresa:</label>
-                     <div class="col-md-10">
-                         <model-list-select
-                                 :disabled="cargando"
-                                 :onchange="changeSelect()"
-                                 name="id_empresa"
-                                 v-model="id_empresa"
-                                 option-value="id"
-                                 option-text="nombre"
-                                 :list="empresas"
-                                 :placeholder="!cargando?'Seleccionar o buscar empresa':'Cargando...'"
-                                 :isError="errors.has(`id_empresa`)">
-                         </model-list-select>
-                     </div>
-                 </div>
-             </div>
-             <div class="col-md-1">
-                 <button @click="conectar" class="btn btn-primary float-right">
-                        <i class="fa fa-plug"></i> Conectar
-                 </button>
-             </div>
-         </div>
-        <span v-if="conectado">
-            <hr />
-            <div class="row">
-                <div class="col-md-12">
-                    <h6><i class="fa fa-filter" ></i>Parámetros de búsqueda:</h6>
+        <div class="row col-md-12">
+            <div class="col-md-6">
+                <div class="form-group row error-content">
+                    <label for="id_empresa" class="col-md-2 col-form-label">Empresa:</label>
+                    <div class="col-md-10">
+                        <model-list-select
+                            :disabled="cargando"
+                            :onchange="changeSelect()"
+                            name="id_empresa"
+                            v-model="id_empresa"
+                            option-value="id"
+                            option-text="nombre"
+                            :list="empresas"
+                            :placeholder="!cargando?'Seleccionar o buscar empresa':'Cargando...'"
+                            :isError="errors.has(`id_empresa`)">
+                        </model-list-select>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-2">
-                     <div class="form-group row error-content">
-                         <label for="ejercicio" class="col-md-6 col-form-label">Ejercicio:</label>
-                         <div class="col-md-6">
-                             <input
-                                     type="text"
-                                     v-validate="{numeric:true, digits: 4}"
-                                     name="ejercicio"
-                                     data-vv-as="ejercicio"
-                                     class="form-control"
-                                     id="ejercicio"
-                                     placeholder="Año"
-                                     v-model="ejercicio"
-                                     :class="{'is-invalid': errors.has('ejercicio')}">
-                             <div class="invalid-feedback" v-show="errors.has('ejercicio')">{{ errors.first('ejercicio') }}</div>
-
-                         </div>
-                     </div>
-                 </div>
-                <div class="col-md-2">
-                     <div class="form-group row error-content">
-                         <label for="periodo" class="col-md-6 col-form-label">Periodo:</label>
-                         <div class="col-md-6">
-                             <input
-                                     type="text"
-                                     v-validate="'between:1,12'"
-                                     name="periodo"
-                                     data-vv-as="Periodo"
-                                     class="form-control"
-                                     id="periodo"
-                                     placeholder="Periodo"
-                                     v-model="periodo"
-                                     :class="{'is-invalid': errors.has('periodo')}">
-                             <div class="invalid-feedback" v-show="errors.has('periodo')">{{ errors.first('periodo') }}</div>
-
-                         </div>
-                     </div>
-                 </div>
-                <div class="col-md-2">
-                     <div class="form-group row error-content">
-                         <label for="tipo_poliza" class="col-md-6 col-form-label">Tipo de Poliza:</label>
-                         <div class="col-md-6">
-                             <select
-                                     class="form-control"
-                                     name="tipo_poliza"
-                                     data-vv-as="Tipo Póliza"
-                                     id="tipo_poliza"
-                                     v-model="tipo_poliza"
-                                     >
-                                    <option value>--Seleccione--</option>
-                                    <option  v-for="(tipo_poliza, index) in tipos_poliza" :value="tipo_poliza.id">
-                                        {{ tipo_poliza.descripcion }}
-                                    </option>
-                             </select>
-                             <div class="invalid-feedback" v-show="errors.has('tipo_poliza')">{{ errors.first('tipo_poliza') }}</div>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-md-2">
-                     <div class="form-group row error-content">
-                         <label for="numero_poliza" class="col-md-6 col-form-label"># Poliza:</label>
-                         <div class="col-md-6">
-                             <input
-                                     type="text"
-                                     name="numero_poliza"
-                                     v-validate="{numeric:true}"
-                                     data-vv-as="Número de Póliza"
-                                     class="form-control"
-                                     id="numero_poliza"
-                                     placeholder="# Póliza"
-                                     v-model="numero_poliza"
-                                     :class="{'is-invalid': errors.has('numero_poliza')}">
-                             <div class="invalid-feedback" v-show="errors.has('numero_poliza')">{{ errors.first('numero_poliza') }}</div>
-
-                         </div>
-                     </div>
-                 </div>
-                <div class="col-md-2">
-                     <div class="form-group row error-content">
-                         <label for="texto" class="col-md-6 col-form-label">Texto:</label>
-                         <div class="col-md-6">
-                             <input
-                                     type="text"
-                                     name="texto"
-                                     data-vv-as="Texto"
-                                     class="form-control"
-                                     id="texto"
-                                     placeholder="Texto"
-                                     v-model="texto"
-                                     :class="{'is-invalid': errors.has('texto')}">
-                             <div class="invalid-feedback" v-show="errors.has('texto')">{{ errors.first('texto') }}</div>
-
-                         </div>
-                     </div>
-                 </div>
-                <div class="col-md-1">
-                    <button @click="getPolizas" v-if="$root.can(['editar_poliza','consultar_poliza'],true)" class="btn btn-primary float-right">
-                        <i class="fa fa-search"></i> Buscar
-                    </button>
-                </div>
-             </div>
-        </span>
-        <span v-if="encontradas">
-            <hr />
-            <div class="row">
-            <div class="col-md-12">
-                <h6><i class="fa fa-th-list" ></i>Resultados de la búsqueda:</h6>
+            <div class="col-md-2">
+                <button @click="conectar" class="btn btn-primary float-right">
+                    <i class="fa fa-plug"></i> Conectar
+                </button>
             </div>
         </div>
-            <div >
-                <datatable v-bind="$data" />
+        <span v-if="encontradas">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <datatable v-bind="$data" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </span>
-
     </span>
 </template>
 
@@ -175,7 +64,6 @@
         components: {Edit, ModelListSelect, Show},
         data() {
             return {
-                tipos_poliza : [{id : 1, descripcion: "Ingreso"},{id : 2, descripcion: "Egreso"},{id : 3, descripcion: "Diario"}],
                 cargando: false,
                 conectando:false,
                 conectado:false,
@@ -184,13 +72,6 @@
                 id_empresa: '',
                 empresas: [],
                 empresa_seleccionada: [],
-                numero_poliza : '',
-                periodo: '',
-                ejercicio: '',
-                tipo_poliza: '',
-                texto:'',
-                tipo_modal:'',
-
                 HeaderSettings: false,
                 columns: [
                     { title: '#', field: 'index', sortable: false },
@@ -324,6 +205,7 @@
                     .then(data => {
                         if(this.empresa_seleccionada.alias_bdd === data){
                             this.conectado = true;
+                            this.getPolizas();
                         }
                     }).finally(() => {
                         this.conectando = false;
