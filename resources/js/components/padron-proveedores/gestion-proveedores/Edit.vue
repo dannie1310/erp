fe596700bb81e20a8d0245346b0474befad89913
DBenitezC <dbenitezc@grupohi.mx>
2020-08-07 19:50:20 -0500

    Se agrego avance de rfc

--- resources/js/components/padron-proveedores/gestion-proveedores/Edit.vue
+++ resources/js/components/padron-proveedores/gestion-proveedores/Edit.vue
@@ -5,7 +5,7 @@
                 <a aria-controls="nav-home" aria-selected="true" class="nav-item nav-link active" data-toggle="tab" href="#nav-home"
                    id="nav-home-tab" role="tab">Datos Proveedor</a><!-- Perfilar  -->
                 <a aria-controls="nav-profile" aria-selected="false" class="nav-item nav-link" data-toggle="tab"
-                   href="#nav-profile" id="nav-profile-tab" role="tab">Datos Empresa Prestadora de Servicios</a>
+                   href="#nav-profile" id="nav-profile-tab" role="tab">Datos Prestadora de Servicios</a>
             </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
