56671efc1a30236fc3ff3a5041640eba1390c31a
DBenitezC <dbenitezc@grupohi.mx>
2020-08-07 19:27:37 -0500

    corregir errores en las routes de padron de proveedores

--- resources/js/components/padron-proveedores/gestion-proveedores/proveedor/Edit.vue
+++ resources/js/components/padron-proveedores/gestion-proveedores/proveedor/Edit.vue
@@ -1,5 +1,30 @@
 <template>
-    <label>proveedor</label>
+    <span>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <label>
+                        RFC:
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                     <input class="form-control"
+                            name="rfc"
+                            data-vv-as="RFC"
+                            v-model="proveedor.rfc"
+                            :class="{'is-invalid':rfcValidate}"
+                            v-validate="{ required: true, regex: /\.(js|ts)$/ }"
+                            id="rfc"
+                            placeholder="RFC" :maxlength="16"/>
+                    <span class="text-danger" v-if="rfcValidate">RFC Inválido</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    </span>
 </template>
 
 <script>
