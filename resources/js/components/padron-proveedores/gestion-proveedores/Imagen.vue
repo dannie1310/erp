0a599aa96a88dd6de128444d3b9ae01b95b88709
DBenitezC <dbenitezc@grupohi.mx>
2020-09-02 16:35:10 -0500

    PDC-81:cambia tamaño de galeria para mostrar las imagenes

--- resources/js/components/padron-proveedores/gestion-proveedores/Imagen.vue
+++ resources/js/components/padron-proveedores/gestion-proveedores/Imagen.vue
@@ -3,7 +3,7 @@
         <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel" v-if="imagenes">
             <div class="carousel-inner">
                 <div v-for="(imagen,i) in imagenes" class="carousel-item" :class="i==0 ? 'active' : ''">
-                    <img :src="imagen['imagen']" :alt="imagen['descripcion']" width="1200" height="800">
+                    <img :src="imagen['imagen']" :alt="imagen['descripcion']" width="800" height="500">
                     <div class="carousel-caption">
                         <h3 style="color:#7BC143">{{imagen['descripcion']}}</h3>
                     </div>
