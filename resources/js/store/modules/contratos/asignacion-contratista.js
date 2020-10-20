fcfa7cd1a8eb8ffc8d520e4f9e31dd85d57e835c
DBenitezC <dbenitezc@grupohi.mx>
2020-10-19 23:03:44 -0500

    ERP-1662: Se inicia el backend para la eliminacion de la asignacion de contratista

--- resources/js/store/modules/contratos/asignacion-contratista.js
+++ resources/js/store/modules/contratos/asignacion-contratista.js
@@ -94,6 +94,47 @@ export default{
                     });
             });
         },
+        eliminar(context, payload) {
+            return new Promise((resolve, reject) => {
+                swal({
+                    title: "Eliminar la Asignación a Contratista",
+                    text: "¿Está seguro de que desea eliminar esta asignación?",
+                    icon: "warning",
+                    closeOnClickOutside: false,
+                    buttons: {
+                        cancel: {
+                            text: 'Cancelar',
+                            visible: true
+                        },
+                        confirm: {
+                            text: 'Si, Eliminar',
+                            closeModal: false,
+                        }
+                    }
+                })
+                    .then((value) => {
+                        if (value) {
+                            axios
+                                .delete(URI + payload.id, {params: payload.params})
+                                .then(r => r.data)
+                                .then(data => {
+                                    swal("Asignación eliminada correctamente", {
+                                        icon: "success",
+                                        timer: 1500,
+                                        buttons: false
+                                    }).then(() => {
+                                        resolve(data);
+                                    })
+                                })
+                                .catch(error => {
+                                    reject(error);
+                                });
+                        } else {
+                            reject();
+                        }
+                    });
+            });
+        },
     },
     getters: {
         asignaciones(state) {
