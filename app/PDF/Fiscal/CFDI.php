7e4e612013399c93eef7d66e930d2349d6328b34
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2020-12-01 16:31:47 -0600

    ERP-2067: Desglose de impuestos por concepto y global en representación PDF del XML del CFDI

--- app/PDF/Fiscal/CFDI.php
+++ app/PDF/Fiscal/CFDI.php
@@ -62,33 +62,40 @@ class CFDI extends Rotation
 
         $this->SetFont('Helvetica', '', 8);
 
-        $this->Cell(2,0.5,"Folio Fiscal: ");
-        $this->Cell(2,0.5,$this->factura["uuid"],0,1);
-        $this->Cell(2,0.5,"Serie y Folio: ");
-        $this->Cell(2,0.5,$this->factura["serie"].$this->factura["folio"],0,1);
+        $this->Cell(2,0.4,"Folio Fiscal: ");
+        $this->Cell(7.25,0.4,$this->factura["uuid"],0,0);
+        $this->Cell(1.2,0.4,"");
+        $this->Cell(4,0.4,utf8_decode("Fecha y Hora de Emisión:"));
+        $this->Cell(5.25,0.4,$this->factura["fecha"]->format("d/m/Y H:i:s"),0,1,"R");
+
+        $this->Cell(2,0.4,"Serie y Folio: ");
+        $this->Cell(7.25,0.4,$this->factura["serie"].$this->factura["folio"],0,0);
+        $this->Cell(1.2,0.4,"");
+        $this->Cell(4,0.4,utf8_decode("Tipo de Comprobante:"));
+        $this->Cell(5.25,0.4,$this->factura["tipo_comprobante"],0,1,"R");
 
         $this->partidasTitle();
     }
 
     public function partidasTitle()
     {
-        $this->SetFont('Arial', '', 8);
+        $this->SetFont('Arial', '', 6);
 
         $this->setXY(1, 3.5);
 
         $this->SetFillColor(180,180,180);
-        $this->SetWidths([0.8,1.5,8,3.2,2,2,2.2]);
-        $this->SetStyles(['DF','DF','DF','DF','DF','DF','DF']);
-        $this->SetRounds(['','','','','','','']);
-        $this->SetRadius([0.2,0,0,0,0,0,0.2]);
+        $this->SetWidths([0.5,1.5,8.2,2,1.5,2,2,2]);
+        $this->SetStyles(['DF','DF','DF','DF','DF','DF','DF','DF']);
+        $this->SetRounds(['','','','','','','','']);
+        $this->SetRadius([0.2,0,0,0,0,0,0,0.2]);
 
-        $this->SetFills(['117,117,117','117,117,117','117,117,117','117,117,117','117,117,117','117,117,117','117,117,117']);
-        $this->SetTextColors(['255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255']);
+        $this->SetFills(['117,117,117','117,117,117','117,117,117','117,117,117','117,117,117','117,117,117','117,117,117','117,117,117']);
+        $this->SetTextColors(['255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255']);
         $this->SetDrawColor(100,100,100);
         $this->SetHeights([0.4]);
-        $this->SetAligns(['C','C','C','C','C','C','C']);
+        $this->SetAligns(['C','C','C','C','C','C','C','C']);
 
-        $this->Row(["#", "Clave SAT", utf8_decode("Descripción"),"Unidad","Cantidad", "Precio Unitario", "Total"]);
+        $this->Row(["#", "Clave SAT", utf8_decode("Descripción"),"Unidad","Cantidad", "Precio Unitario", "Total", "Descuento"]);
 
     }
 
@@ -97,20 +104,77 @@ class CFDI extends Rotation
         $i = 1;
         foreach($this->factura["conceptos"] as $partida){
             $this->SetDrawColor(100,100,100);
-            $this->SetFont('Arial', '', 8);
-            $this->SetAligns(['C','L','L','C','R','R','R']);
-            $this->SetTextColors(['0,0,0','0,0,0','0,0,0','0,0,0','0,0,0','0,0,0','0,0,0']);
-            $this->SetFills(['255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255']);
+            $this->SetFont('Arial', '', 7);
+            $this->SetAligns(['C','L','L','C','R','R','R','R']);
+            $this->SetTextColors(['0,0,0','0,0,0','0,0,0','0,0,0','0,0,0','0,0,0','0,0,0','0,0,0']);
+            $this->SetFills(['255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255','255,255,255']);
             $this->Row([
                 $i,
-
-
                 utf8_decode($partida["clave_prod_serv"]),
                 utf8_decode($partida["descripcion"]),
                 $partida["unidad"],
                 $partida["cantidad"],
                 "$".number_format($partida["valor_unitario"],2),
                 "$".number_format($partida["importe"],2),
+                "$".number_format($partida["descuento"],2),
+            ]);
+            $this->trasladosConcepto($partida["traslados"]);
+            $this->retencionesConcepto($partida["retenciones"]);
+            $i++;
+        }
+
+    }
+
+    public function trasladosConcepto($traslados)
+    {
+        $i = 1;
+        foreach($traslados as $traslado){
+            $this->SetWidths([19.7]);
+            $this->SetDrawColor(100,100,100);
+            $this->SetFont('Arial', '', 7);
+            $this->SetAligns(['L']);
+            $this->SetTextColors(['0,0,0']);
+            $this->SetFills(['255,255,255']);
+            $impuesto = ($traslado["impuesto"]=="002")?"IVA":(($traslado["impuesto"]=="001")?"ISR":($traslado["impuesto"]=="003")?"IEPS":"");
+            $this->Row([
+                "-Impuesto Trasladado-    Base: ".
+                number_format($traslado["base"],2).
+                "   Impuesto: ".
+                $impuesto.
+                "   Tipo Factor: ".
+                $traslado["tipo_factor"].
+                "   Tasa o Cuota: ".
+                $traslado["tasa_o_cuota"].
+                "   Importe: ".
+                number_format($traslado["importe"],2),
+            ]);
+            $i++;
+        }
+
+    }
+
+    public function retencionesConcepto($retenciones)
+    {
+        $i = 1;
+        foreach($retenciones as $retencion){
+            $this->SetWidths([19.7]);
+            $this->SetDrawColor(100,100,100);
+            $this->SetFont('Arial', '', 7);
+            $this->SetAligns(['L']);
+            $this->SetTextColors(['0,0,0']);
+            $this->SetFills(['255,255,255']);
+            $impuesto = ($retencion["impuesto"]=="002")?"IVA":(($retencion["impuesto"]=="001")?"ISR":($retencion["impuesto"]=="003")?"IEPS":"");
+            $this->Row([
+                "-Impuesto Retenido-    Base: ".
+                number_format($retencion["base"],2).
+                "   Impuesto: ".
+                $impuesto.
+                "   Tipo Factor: ".
+                $retencion["tipo_factor"].
+                "   Tasa o Cuota: ".
+                $retencion["tasa_o_cuota"].
+                "   Importe: ".
+                number_format($retencion["importe"],2),
             ]);
             $i++;
         }
@@ -119,23 +183,36 @@ class CFDI extends Rotation
 
     public function detallesPagoImportes()
     {
+        $this->ln(0.2);
         $this->SetFont('Arial', '', 8);
-        $this->Cell(2,0.5,"Moneda y TC: ");
-        $this->Cell(13.5,0.5,$this->factura["moneda"]."-".$this->factura["tipo_cambio"]);
-        $this->Cell(2,0.5,"Subtotal: ", 0,0,"R");
-        $this->Cell(2.2,0.5,"$".number_format($this->factura["subtotal"],2),0,1,"R");
+        $this->Cell(2,0.4,"Moneda y TC: ");
+        $this->Cell(13.5,0.4,$this->factura["moneda"]."-".$this->factura["tipo_cambio"]);
+        $this->Cell(2,0.4,"Subtotal: ", 0,0,"R");
+        $this->Cell(2.2,0.4,"$".number_format($this->factura["subtotal"],2),0,1,"R");
 
-        $this->Cell(15.5,0.5,"");
-        $this->Cell(2,0.5,"Impuestos Trasladados: ", 0,0,"R");
-        $this->Cell(2.2,0.5,"$".number_format($this->factura["total_impuestos_trasladados"],2),0,1,"R");
+        if($this->factura["descuento"]>0){
+            $this->Cell(15.5,0.4,"");
+            $this->Cell(2,0.4,"Descuento: ", 0,0,"R");
+            $this->Cell(2.2,0.4,"$".number_format($this->factura["descuento"],2),0,1,"R");
+        }
 
-        $this->Cell(15.5,0.5,"");
-        $this->Cell(2,0.5,"Impuestos Retenidos: ", 0,0,"R");
-        $this->Cell(2.2,0.5,"$".number_format($this->factura["total_impuestos_retenidos"],2),0,1,"R");
+        foreach($this->factura["traslados"] as $traslado){
+            $impuesto = ($traslado["impuesto"]=="002")?"IVA":(($traslado["impuesto"]=="001")?"ISR":($traslado["impuesto"]=="003")?"IEPS":"");
+            $this->Cell(15.5,0.4,"");
+            $this->Cell(2,0.4,$impuesto." Trasladado: ", 0,0,"R");
+            $this->Cell(2.2,0.4,"$".number_format($traslado["importe"],2),0,1,"R");
+        }
 
-        $this->Cell(15.5,0.5,"");
-        $this->Cell(2,0.5,"Total: ", 0,0,"R");
-        $this->Cell(2.2,0.5,"$".number_format($this->factura["total"],2),0,1,"R");
+        foreach($this->factura["retenciones"] as $retencion){
+            $impuesto = ($retencion["impuesto"]=="002")?"IVA":(($retencion["impuesto"]=="001")?"ISR":($retencion["impuesto"]=="003")?"IEPS":"");
+            $this->Cell(15.5,0.4,"");
+            $this->Cell(2,0.4,$impuesto." Retenido: ", 0,0,"R");
+            $this->Cell(2.2,0.4,"$".number_format($retencion["importe"],2),0,1,"R");
+        }
+
+        $this->Cell(15.5,0.4,"");
+        $this->Cell(2,0.4,"Total: ", 0,0,"R");
+        $this->Cell(2.2,0.4,"$".number_format($this->factura["total"],2),0,1,"R");
     }
 
 
