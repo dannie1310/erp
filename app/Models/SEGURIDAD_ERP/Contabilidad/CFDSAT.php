52cc9a037e4341bfc7c8d3815c06f95799b5adc9
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2020-12-02 11:04:45 -0600

    ERP-2070: Se muestra subtotal, descuento, impuestos retenidos e impuestos trasladados en index de CFDI

--- app/Models/SEGURIDAD_ERP/Contabilidad/CFDSAT.php
+++ app/Models/SEGURIDAD_ERP/Contabilidad/CFDSAT.php
@@ -31,6 +31,7 @@ class CFDSAT extends Model
         ,"folio"
         ,"fecha"
         ,"total_impuestos_trasladados"
+        ,"total_impuestos_retenidos"
         ,"tasa_iva"
         ,"importe_iva"
         ,"descuento"
@@ -165,6 +166,26 @@ class CFDSAT extends Model
 
     public function getTotalFormatAttribute()
     {
-        return '$ ' . number_format(abs($this->total),2);
+        return '$ ' . number_format(($this->total),2);
+    }
+
+    public function getSubtotalFormatAttribute()
+    {
+        return '$ ' . number_format(($this->subtotal),2);
+    }
+
+    public function getDescuentoFormatAttribute()
+    {
+        return '$ ' . number_format(($this->descuento),2);
+    }
+
+    public function getTotalImpuestosRetenidosFormatAttribute()
+    {
+        return '$ ' . number_format(($this->total_impuestos_retenidos),2);
+    }
+
+    public function getTotalImpuestosTrasladadosFormatAttribute()
+    {
+        return '$ ' . number_format(($this->total_impuestos_trasladados),2);
     }
 }
