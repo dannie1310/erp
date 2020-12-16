46beef2de60615c83e9ff8c5a7310aced6cd0225
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2020-12-15 22:47:11 -0600

    ERP-2087: Se ponen firmas especiales para el formato de estimaciones de acarreos del proyecto presa libertad

---
+++ app/Models/CADECO/Acarreos/ConciliacionEstimacion.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Models\CADECO\Acarreos;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConciliacionEstimacion extends Model
+{
+    protected $connection = 'cadeco';
+    protected $table = 'Acarreos.conciliacion_estimacion';
+
+}
