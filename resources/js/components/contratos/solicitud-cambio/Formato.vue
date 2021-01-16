1ab082c1b6ec29af7d54b8788e042079e5013f7e
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2021-01-15 21:55:41 -0600

    ERP-2093: Se reagrega consulta de PDF de solicitud en componentes Show y Aplicar

--- resources/js/components/contratos/solicitud-cambio/Formato.vue
+++ resources/js/components/contratos/solicitud-cambio/Formato.vue
@@ -12,7 +12,10 @@
                     <div class="modal-body modal-lg" style="height: 800px" ref="body">
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                            <i class="fa fa-times-circle"></i>
+                            Cerrar
+                        </button>
                     </div>
                 </div>
             </div>
