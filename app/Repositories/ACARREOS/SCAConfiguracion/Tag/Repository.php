79fca63d368f94f8b7443ba10dd5a2487b8b3650
DBenitezC <dbenitezc@grupohi.mx>
2021-01-28 18:07:29 -0600

    SCAE-39:Se obtiene la descarga de catalogos de aplicacion registro tags

--- app/Repositories/ACARREOS/SCAConfiguracion/Tag/Repository.php
+++ app/Repositories/ACARREOS/SCAConfiguracion/Tag/Repository.php
@@ -4,6 +4,7 @@
 namespace App\Repositories\ACARREOS\SCAConfiguracion\Tag;
 
 
+use App\Models\ACARREOS\SCA_CONFIGURACION\Proyecto;
 use App\Models\ACARREOS\SCA_CONFIGURACION\Tag;
 use App\Repositories\RepositoryInterface;
 
@@ -13,5 +14,22 @@ class Repository extends \App\Repositories\Repository implements RepositoryInter
     {
         parent::__construct($model);
         $this->model = $model;
+    }
+
+    /**
+     * Obtener catálogo de proyectos.
+     * @return mixed
+     */
+    public function getProyectos()
+    {
+        $proyectos = Proyecto::select('id_proyecto', 'descripcion')->get()->toArray();
+        $proyectos_disponibles = array();
+        foreach ($proyectos as $key => $proyecto)
+        {
+            $proyecto['id_proyecto'] = (String) $proyecto['id_proyecto'];
+            $proyecto['descripcion'] = (String) utf8_encode($proyecto['descripcion']);
+            $proyectos_disponibles[$key] = $proyecto;
+        }
+        return $proyectos_disponibles;
     }
 }
--- app/Services/ACARREOS/Configuracion/TagService.php
+++ app/Services/ACARREOS/Configuracion/TagService.php
@@ -5,7 +5,11 @@ namespace App\Services\ACARREOS\Configuracion;
 
 
 
+use App\Models\ACARREOS\SCA_CONFIGURACION\PermisoAltaTag;
+use App\Models\ACARREOS\SCA_CONFIGURACION\Proyecto;
 use App\Models\ACARREOS\SCA_CONFIGURACION\Tag;
+use App\Models\ACARREOS\SCA_CONFIGURACION\UsuarioProyecto;
+use App\Models\IGH\Usuario;
 use App\Repositories\ACARREOS\SCAConfiguracion\Tag\Repository;
 use Illuminate\Support\Facades\DB;
 
@@ -49,6 +53,37 @@ class TagService
      */
     public function getCatalogo($data)
     {
-        dd("AQUI");
+        /**
+         * Buscar usuario con el proyecto ultimo asociado al usuario.
+         */
+        $id_usuario = Usuario::where('usuario', $data['usuario'])->where('clave',  md5($data['clave']))->pluck('idusuario');
+        if(count($id_usuario) == 0)
+        {
+            return json_encode(array("error" => "Error al iniciar sesión, su usuario y/o clave son incorrectos."));
+        }
+
+        /**
+         * Revisar privilegios para dar de alta de tags
+         */
+        $permiso = PermisoAltaTag::selectRaw('if( vigencia > NOW() OR vigencia is null, 1,0) AS valido')->where('idusuario', $id_usuario)->first();
+        if($permiso->valido == 0)
+        {
+            return json_encode(array("error" =>  "No tiene los privilegios para dar de alta tags en los proyectos."));
+        }
+
+        /**
+         * Obtener datos de configuración del usuario.
+         */
+        $usuario = UsuarioProyecto::activo()->ordenarProyectos()->where('id_usuario_intranet', $id_usuario);
+        if(is_null($usuario->first()))
+        {
+            return json_encode(array("error" =>  "Error al obtener los datos de configuración"));
+        }
+
+        return [
+            'IdUsuario' => (String) auth()->id(),
+            'Nombre' => $usuario->first()->usuario->nombre_completo,
+            'proyectos' => $this->repository->getProyectos()
+        ];
     }
 }
