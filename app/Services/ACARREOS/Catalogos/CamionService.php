642fff8ca5f8e16a8cbfb622c2f6ad448bca4bbd
DBenitezC <dbenitezc@grupohi.mx>
2021-02-04 20:38:20 -0600

    SCAE-11: Se avanza en el logueo

--- app/Services/ACARREOS/Catalogos/CamionService.php
+++ app/Services/ACARREOS/Catalogos/CamionService.php
@@ -5,6 +5,8 @@ namespace App\Services\ACARREOS\Catalogos;
 
 
 use App\Models\ACARREOS\Camion;
+use App\Models\ACARREOS\SCA_CONFIGURACION\UsuarioProyecto;
+use App\Models\IGH\Usuario;
 use App\Repositories\ACARREOS\Camion\Repository;
 use Illuminate\Support\Facades\DB;
 
@@ -51,11 +53,32 @@ class CamionService
         /**
          * Buscar usuario con el proyecto ultimo asociado al usuario.
          */
-        $usuario = $this->repository->usuarioProyecto($data['usuario'], $data['clave']);
+        $id_usuario = Usuario::where('usuario', $data['usuario'])->where('clave',  md5($data['clave']))->pluck('idusuario');
+        if(count($id_usuario) == 0)
+        {
+            return json_encode(array("error" => "Error al iniciar sesión, su usuario y/o clave son incorrectos."));
+        }
+        $usuario = UsuarioProyecto::activo()->ordenarProyectos()->where('id_usuario_intranet', $id_usuario);
+        if(is_null($usuario->first()))
+        {
+            return json_encode(array("error" =>  "Error al obtener los datos del proyecto. Probablemente el usuario no tenga asignado ningun proyecto."));
+        }
+
         /**
          * Se realiza conexión con la base de datos de acarreos.
          */
         $this->conexionAcarreos($usuario->first()->proyecto->base_datos);
+
+        /**
+         * Revision de permisos
+         */
+        /**
+         * Validar si el usuario tiene el role de checador.
+         */
+        $eschecador = $this->repository->esChecador($usuario->first());
+        if (!$eschecador) {
+            return json_encode(array("error" => "El usuario no tiene perfil de CHECADOR favor de solicitarlo."));
+        }
 
     }
 }
