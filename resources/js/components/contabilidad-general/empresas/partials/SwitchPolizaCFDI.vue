70f9a621125f512375559018655fb8a7986a045e
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2021-02-15 22:48:25 -0600

    ERP-2124: 30m Se finaliza la actualización de las propiedades de las empresas por medio de switchs

---
+++ resources/js/components/contabilidad-general/empresas/partials/SwitchPolizaCFDI.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="custom-control custom-switch" >
+        <input type="checkbox" class="custom-control-input button"  :id="`switch_p${value.id}`" v-model="value.poliza_cfdi" @change="update" >
+        <label class="custom-control-label" :for="`switch_p${value.id}`"></label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SwitchPolizaCFDI",
+    props: ['value'],
+    data() {
+        return {
+        }
+    },
+
+    methods: {
+        update(){
+            return this.$store.dispatch('seguridad/lista-empresas/updateDirecto', {
+                id: this.value.id,
+                params:{
+                    SincronizacionPolizasCFDI: this.value.poliza_cfdi ? 1 :0
+                }
+            }).then(data => {
+                this.$store.commit('contabilidadGeneral/empresa/UPDATE_EMPRESA', data);
+            })
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
