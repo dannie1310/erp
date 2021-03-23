baf78fc6e5da3a1b414053680dd3a34832518cb7
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2021-03-23 17:34:40 -0600

    ERPHI-126: 15m Cambio en cuerpos de correo de notificaciones para atender observaciones de LH

--- resources/views/emails/rechazo_solicitud_recepcion_cfdi.blade.php
+++ resources/views/emails/rechazo_solicitud_recepcion_cfdi.blade.php
@@ -12,41 +12,46 @@
     </head>
     <div id="app">
         Se le informa que {{$solicitud->usuarioRechazo->nombre_completo}} ha rechazado una solicitud de recepción de CFDI para su proyecto.
+        <br>
+        <br>
         <div class="card" >
-            <div class="card-header">
-                <h5>Datos de la Solicitud de Recepción</h5>
-            </div>
             <div class="card-body">
                 <span>
                     <div class="row">
                         <div class="col-md-4">
                             <div class="form-group">
-                                <label >Indetificador:</label>
-                                {{$solicitud->id}}
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label >Folio para proveedor:</label>
-                                {{$solicitud->numero_folio}}
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group">
                                 <label >Proyecto:</label>
-                                {{$solicitud->base_datos}} ({{$solicitud->obra->nombre}})
+                                <b>{{$solicitud->obra->nombre}}</b>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label >Total:</label>
+                                <b>{{$solicitud->cfdi->total_format}}</b>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                            <label >Emisión:</label>
+                                {{$solicitud->cfdi->fecha_format}}
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                            <label >Serie y Folio:</label>
+                                {{$solicitud->cfdi->serie}} {{$solicitud->cfdi->folio}}
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label >Folio de Solicitud:</label>
+                                {{$solicitud->numero_folio}}
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div class="form-group">
                                 <label >Contacto HI:</label>
                                 {{$solicitud->contacto}}
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label >Correo para enviar notificaciones:</label>
-                                {{$solicitud->correo_notificaciones}}
                             </div>
                         </div>
                     </div>
@@ -74,131 +79,6 @@
                                 Motivo Rechazo:
                             </label>
                             {{$solicitud->motivo_rechazo}}
-                        </div>
-                    </div>
-                </span>
-            </div>
-        </div>
-        <hr>
-        <div class="card" >
-            <div class="card-header">
-                <h5>Datos del CFDI</h5>
-            </div>
-            <div class="card-body">
-                <span>
-
-                    <div class="row">
-                        <div class="col-md-2">
-                            <div class="form-group">
-                            <label >Emisión:</label>
-                                {{$solicitud->cfdi->fecha_format}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                            <label >Serie y Folio:</label>
-                                {{$solicitud->cfdi->serie}} {{$solicitud->cfdi->folio}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                            <label >Tipo:</label>
-                                {{$solicitud->cfdi->tipo_comprobante}}
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="form-group">
-                            <label >UUID:</label>
-                                {{$solicitud->cfdi->uuid}}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">
-                            <div class="form-group">
-                            <label >Empresa:</label>
-                                {{$solicitud->cfdi->empresa->razon_social}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label >RFC:</label>
-                                {{$solicitud->cfdi->empresa->rfc}}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label >Descuento:</label>
-                                {{$solicitud->cfdi->descuento_format}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label >Impuestos Retenidos:</label>
-                                {{$solicitud->cfdi->total_impuestos_retenidos_format}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label >Impuestos Trasladados:</label>
-                                {{$solicitud->cfdi->total_impuestos_trasladados_format}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label >Total:</label>
-                                {{$solicitud->cfdi->total_format}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label >Moneda:</label>
-                                {{$solicitud->cfdi->moneda}}
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label >Tipo de Cambio:</label>
-                                {{$solicitud->cfdi->tipo_cambio}}
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
-                    <h5>Conceptos del CFDI</h5>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th class="index_corto">#</th>
-                                        <th class="no_parte">Clave Producto / Servicio</th>
-                                        <th>Descripción</th>
-                                        <th>Clave Unidad</th>
-                                        <th>Unidad</th>
-                                        <th>Cantidad</th>
-                                        <th>Descuento</th>
-                                        <th>Valor Unitario</th>
-                                        <th>Importe</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($solicitud->cfdi->conceptos as $concepto)
-                                        <tr >
-                                            <td>{{1}}</td>
-                                            <td>{{$concepto->clave_prod_serv}}</td>
-                                            <td>{{$concepto->descripcion}}</td>
-                                            <td>{{$concepto->clave_unidad}}</td>
-                                            <td>{{$concepto->unidad}}</td>
-                                            <td style="text-align: right">{{$concepto->cantidad_format}}</td>
-                                            <td style="text-align: right">{{$concepto->descuento_format}}</td>
-                                            <td style="text-align: right">{{$concepto->valor_unitario_format}}</td>
-                                            <td style="text-align: right">{{$concepto->importe_format}}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </span>
