f5e3c2d0a526b3aa7990e6d6c0d0a2a5f9ef1886
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2021-04-27 23:30:01 -0500

    ERPHI-163: 15m aplicación de filtros en index de cuentas contables con saldos negativos

--- app/Services/SEGURIDAD_ERP/Contabilidad/CuentaSaldoNegativoService.php
+++ app/Services/SEGURIDAD_ERP/Contabilidad/CuentaSaldoNegativoService.php
@@ -29,6 +29,21 @@ class CuentaSaldoNegativoService
 
     public function paginate($data)
     {
+        if (isset($data['base_datos'])) {
+            $this->repository->where([['base_datos', 'LIKE', '%' . $data['base_datos'] . '%']]);
+        }
+        if (isset($data['nombre_empresa'])) {
+            $this->repository->where([['nombre_empresa', 'LIKE', '%' . $data['nombre_empresa'] . '%']]);
+        }
+        if (isset($data['codigo_cuenta'])) {
+            $this->repository->where([['codigo_cuenta', 'LIKE', '%' . $data['codigo_cuenta'] . '%']]);
+        }
+        if (isset($data['tipo'])) {
+            $this->repository->where([['tipo', '=', $data['tipo']]]);
+        }
+        if (isset($data['nombre_cuenta'])) {
+            $this->repository->where([['nombre_cuenta', 'LIKE', '%' . $data['nombre_cuenta'] . '%']]);
+        }
         return $this->repository->paginate($data);
     }
 
