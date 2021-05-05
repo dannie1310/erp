b2e5717acc4a8b20bff7c1b27cd4cbed16b90da5
DBenitezC <dbenitezc@grupohi.mx>
2021-05-05 18:47:25 -0500

    ERPHI-203: Se disminuye minimo de caracteres requeridos para el nombre de contacto en el padron de proveedores

--- resources/js/components/padron-proveedores/gestion-proveedores/Create.vue
+++ resources/js/components/padron-proveedores/gestion-proveedores/Create.vue
@@ -296,7 +296,7 @@
                                        :data-vv-as="`'Nombre ${i + 1}'`"
                                        v-model="contacto.nombre"
                                        :class="{'is-invalid': errors.has(`nombre[${i}]`)}"
-                                       v-validate="{ required: true, min:10 }"
+                                       v-validate="{ required: true, min:4 }"
                                        :id="`nombre[${i}]`"
                                        :maxlength="250"/>
                                 <div class="invalid-feedback" v-show="errors.has(`nombre[${i}]`)">{{ errors.first(`nombre[${i}]`) }}</div>
