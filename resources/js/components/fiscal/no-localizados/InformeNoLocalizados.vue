49177f4e31047d939ca9a7dde0f3d93b591c1c2c
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2021-05-14 16:16:33 -0500

    ERPHI-15: 1m Se retira fila de etiqueta y columna de estado No Localizado por ser redundantes

--- resources/js/components/fiscal/no-localizados/InformeNoLocalizados.vue
+++ resources/js/components/fiscal/no-localizados/InformeNoLocalizados.vue
@@ -28,16 +28,11 @@
                                 <template v-for="(partidas, j) in tipo">
                                      <template v-if="partidas.tipo == 'titulo'">
                                          <tr>
-                                            <td colspan="12" style="background-color: #fff" ></td>
+                                            <td colspan="11" style="background-color: #fff" ></td>
                                         </tr>
-                                        <tr>
-                                            <td colspan="12" :style="{'background-color': partidas.bg_color_hex, 'color': partidas.color_hex}" >
-                                                {{partidas.etiqueta}}
-                                            </td>
-                                        </tr>
+
                                         <tr style="background-color: #757575; color:#FFF; text-align:center" >
                                             <td class="index_corto">#</td>
-                                            <td>Estatus</td>
                                             <td>RFC</td>
                                             <td>Razón Social</td>
                                             <td>Fecha Presunto</td>
@@ -52,7 +47,6 @@
                                     </template>
                                     <tr v-else-if="partidas.tipo == 'partida'" >
                                         <td class="index_corto">{{partidas.indice}}</td>
-                                        <td>{{partidas.estatus}}</td>
                                         <td>{{partidas.rfc}}</td>
                                         <td>{{partidas.razon_social}}</td>
                                         <td>{{partidas.fecha_presunto}}</td>
@@ -67,7 +61,6 @@
 
                                     <tr v-else :style="{'background-color': partidas.bg_color_hex, 'color': partidas.color_hex}"  >
                                         <td class="index_corto">{{partidas.contador}}</td>
-                                        <td></td>
                                         <td></td>
                                         <td>{{partidas.etiqueta}}</td>
                                         <td></td>
