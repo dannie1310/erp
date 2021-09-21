49cbea539855e404101881bba38fbf516a693bc3
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2021-09-20 19:30:56 -0500

    ERPHI-556: 15m 47pmd se corrige vista de cotización para mostrar correctamente los precios despues de descuento

--- resources/js/components/portal-proveedor/cotizacion/partials/PartialShow.vue
+++ resources/js/components/portal-proveedor/cotizacion/partials/PartialShow.vue
@@ -46,11 +46,11 @@
                                     <td class="numerico">
                                         {{(parseFloat(partida.descuento)).formatMoney(2,'.',',')}}
                                     </td>
-                                    <td style="text-align:right;">{{'$' + parseFloat((partida.cantidad) * partida.precio_unitario).formatMoney(2,'.',',')}}</td>
+                                    <td style="text-align:right;">{{'$'+parseFloat(getPrecio(partida)).formatMoney(2,'.',',') }}</td>
                                     <td style="width:120px;" >
                                         {{ partida.moneda.nombre}}
                                     </td>
-                                    <td style="text-align:right;" v-if="multiples_monedas">{{partida.precio_total_moneda}}</td>
+                                    <td style="text-align:right;"  v-if="multiples_monedas">{{getPrecioTotal(getPrecio(partida), partida.id_moneda)}}</td>
                                     <td style="width:200px;">
                                         {{partida.observacion}}
                                     </td>
@@ -638,6 +638,34 @@ export default {
             }
             if(this.libra_seleccionado){
                 this.ancho_tabla_detalle += 150;
+            }
+        },
+        getPrecio(partida){
+            if(partida.precio_unitario){
+                return partida.precio_unitario * partida.cantidad- (partida.precio_unitario * partida.cantidad * (partida.descuento ? partida.descuento : 0) / 100);
+            }
+            return '0.00';
+        },
+        getPrecioTotal(precio, moneda) {
+            if(moneda == undefined)
+            {
+                return '$0.00'
+            }
+            if(moneda === 1)
+            {
+                return '$'+parseFloat(precio != undefined ? precio : 1).formatMoney(2,'.',',')
+            }
+            if(moneda === 2)
+            {
+                return '$'+parseFloat(precio != undefined ? precio * this.dolar : this.dolar).formatMoney(2,'.',',')
+            }
+            if(moneda === 3)
+            {
+                return '$'+parseFloat(precio != undefined ? precio * this.euro : this.euro).formatMoney(2,'.',',')
+            }
+            if(moneda === 4)
+            {
+                return '$'+parseFloat(precio != undefined ? precio * this.libra : this.libra).formatMoney(2,'.',',')
             }
         },
     },
