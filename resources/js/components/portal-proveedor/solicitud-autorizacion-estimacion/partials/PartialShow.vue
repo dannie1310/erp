6670f6308e44f7d39fccb6e7fb4a7f9e313b00d1
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2021-12-15 02:45:38 -0600

    ERPHI-944: 15m 22pmd modificar estilo para componente PartialShow.vue

--- resources/js/components/portal-proveedor/solicitud-autorizacion-estimacion/partials/PartialShow.vue
+++ resources/js/components/portal-proveedor/solicitud-autorizacion-estimacion/partials/PartialShow.vue
@@ -9,38 +9,36 @@
                 </div>
             </div>
         </div>
-        <div class="row" v-if="!cargando">
-            <div class="col-md-12" v-if="estimacion">
-                <resumen v-bind:estimacion="estimacion" v-bind:base="base" />
+        <div v-if="!cargando">
+            <div class="row" >
+                <div class="col-md-12" v-if="estimacion">
+                    <resumen v-bind:estimacion="estimacion" v-bind:base="base" />
+                </div>
             </div>
-            <div class="col-md-12" >
-                <datos v-bind:estimacion="estimacion" />
+            <div class="row" >
+                <div class="col-md-12" >
+                    <datos v-bind:estimacion="estimacion" />
+                </div>
             </div>
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="form-check form-check-inline">
-                            <input v-model="columnas" class="form-check-input" type="checkbox" value="contratado" id="contratado">
-                            <label class="form-check-label" for="contratado">Contratado</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input v-model="columnas" class="form-check-input" type="checkbox" id="avance-volumen" value="avance-volumen">
-                            <label class="form-check-label" for="avance-volumen">Avance Volumen</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input v-model="columnas" class="form-check-input" type="checkbox" id="avance-importe" value="avance-importe">
-                            <label class="form-check-label" for="avance-importe">Avance Importe</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input v-model="columnas" class="form-check-input" type="checkbox" id="saldo" value="saldo">
-                            <label class="form-check-label" for="saldo">Saldo</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input v-model="columnas" class="form-check-input" type="checkbox" id="destino" value="destino">
-                            <label class="form-check-label" for="destino">Destino</label>
-                        </div>
+            <div class="row" >
+                <div class="col-md-12">
+                    <div class="form-check form-check-inline">
+                        <input v-model="columnas" class="form-check-input" type="checkbox" value="contratado" id="contratado">
+                        <label class="form-check-label" for="contratado">Contratado</label>
                     </div>
-		        </div>
+                    <div class="form-check form-check-inline">
+                        <input v-model="columnas" class="form-check-input" type="checkbox" id="avance-volumen" value="avance-volumen">
+                        <label class="form-check-label" for="avance-volumen">Avance Volumen</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input v-model="columnas" class="form-check-input" type="checkbox" id="avance-importe" value="avance-importe">
+                        <label class="form-check-label" for="avance-importe">Avance Importe</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input v-model="columnas" class="form-check-input" type="checkbox" id="saldo" value="saldo">
+                        <label class="form-check-label" for="saldo">Saldo</label>
+                    </div>
+                </div>
             </div>
             <div class="col-md-12">
                 <div class="row">
@@ -48,31 +46,29 @@
                         <table id="tabla-conceptos">
                             <thead>
                                 <tr>
-                                    <th rowspan="2">Clave</th>
+                                    <th rowspan="2" class="c120">Clave</th>
                                     <th rowspan="2">Concepto</th>
-                                    <th rowspan="2">UM</th>
+                                    <th rowspan="2" class="c100">Unidad</th>
                                     <th style="display: none" colspan="2" class="contratado">Contratado</th>
                                     <th style="display: none" colspan="3" class="avance-volumen">Avance Volumen</th>
                                     <th style="display: none" colspan="2" class="avance-importe">Avance Importe</th>
                                     <th style="display: none" colspan="2" class="saldo">Saldo</th>
-                                    <th colspan="4">Esta Estimación</th>
-                                    <th style="display: none" class="destino">Distribución</th>
+                                    <th colspan="4">Esta Solicitud</th>
                                 </tr>
                                 <tr>
-                                    <th style="display: none" class="contratado">Volumen</th>
-                                    <th style="display: none" class="contratado">P.U.</th>
-                                    <th style="display: none" class="avance-volumen">Anterior</th>
-                                    <th style="display: none" class="avance-volumen">Acumulado</th>
-                                    <th style="display: none" class="avance-volumen">%</th>
-                                    <th style="display: none" class="avance-importe">Anterior</th>
-                                    <th style="display: none" class="avance-importe">Acumulado</th>
-                                    <th style="display: none" class="saldo">Volumen</th>
-                                    <th style="display: none" class="saldo">Importe</th>
-                                    <th>Volumen</th>
-                                    <th>%</th>
-                                    <th>P.U.</th>
-                                    <th>Importe</th>
-                                    <th style="display: none" class="destino">Destino</th>
+                                    <th style="display: none" class="contratado c100">Volumen</th>
+                                    <th style="display: none" class="contratado c100">P.U.</th>
+                                    <th style="display: none" class="avance-volumen c100">Anterior</th>
+                                    <th style="display: none" class="avance-volumen c100">Acumulado</th>
+                                    <th style="display: none" class="avance-volumen c100">%</th>
+                                    <th style="display: none" class="avance-importe c100">Anterior</th>
+                                    <th style="display: none" class="avance-importe c100">Acumulado</th>
+                                    <th style="display: none" class="saldo c100">Volumen</th>
+                                    <th style="display: none" class="saldo c100">Importe</th>
+                                    <th class="c100">Volumen</th>
+                                    <th class="c100">%</th>
+                                    <th class="c100">P.U.</th>
+                                    <th class="c100">Importe</th>
                                 </tr>
                             </thead>
                             <tbody v-for="(concepto, i) in estimacion.subcontrato.partidas">
@@ -95,7 +91,6 @@
                                     <td></td>
                                     <td></td>
                                     <td></td>
-                                    <td style="display: none" class="destino"/>
                                 </tr>
                                 <tr v-else>
                                     <td :title="concepto.clave">{{ concepto.clave }}</td>
@@ -116,8 +111,7 @@
                                     <td class="numerico">{{parseFloat(concepto.cantidad_estimacion).formatMoney(2)}}</td>
                                     <td class="numerico">{{parseFloat(concepto.porcentaje_estimado).formatMoney(2)}}</td>
                                     <td class="numerico">{{ concepto.precio_unitario_subcontrato_format}}</td>
-                                    <td class="numerico">{{parseFloat(concepto.importe_estimacion).formatMoney(2)}}</td>
-                                    <td style="display: none" class="destino" :title="concepto.destino_path">{{ concepto.destino_path }}</td>
+                                    <td class="numerico">${{parseFloat(concepto.importe_estimacion).formatMoney(2)}}</td>
                                 </tr>
                             </tbody>
                         </table>
@@ -185,81 +179,90 @@
 </script>
 
 <style scoped>
-    table#tabla-conceptos {
-        word-wrap: unset;
-        width: 100%;
-        background-color: white;
-        border-color: transparent;
-        border-collapse: collapse;
-        clear: both;
-    }
+table#tabla-conceptos {
+    word-wrap: unset;
+    width: 100%;
+    background-color: white;
+    border-color: transparent;
+    border-collapse: collapse;
+    clear: both;
+    font-size: 13px;
+}
 
-    table thead th
-    {
-        padding: 0.2em;
-        border: 1px solid #666;
-        background-color: #333;
-        color: white;
-        font-weight: normal;
-        overflow: hidden;
-        text-align: center;
-    }
+table thead th
+{
+    padding: 0.2em;
+    border: 1px solid #ccc;
+    background-color: #f2f4f5;
+    color: #242424;
+    /*border-right: 1px solid #ccc;*/
+    font-weight: normal;
+    overflow: hidden;
+    text-align: center;
+}
 
-    table thead th {
-        text-align: center;
-    }
-    table tbody tr
-    {
-        border-width: 0 1px 1px 1px;
-        border-style: none solid solid solid;
-        border-color: white #CCCCCC #CCCCCC #CCCCCC;
-    }
-    table tbody td,
-    table tbody th
-    {
-        border-right: 1px solid #ccc;
-        color: #242424;
-        line-height: 20px;
-        overflow: hidden;
-        padding: 1px 5px;
-        text-align: left;
-        text-overflow: ellipsis;
-        -o-text-overflow: ellipsis;
-        -ms-text-overflow: ellipsis;
-        white-space: nowrap;
-    }
+table thead th {
+    text-align: center;
+}
+table tbody tr
+{
+    border-width: 0 1px 1px 1px;
+    border-style: none solid solid solid;
+    border-color: white #CCCCCC #CCCCCC #CCCCCC;
+}
+table tbody td,
+table tbody th
+{
+    border-right: 1px solid #ccc;
+    color: #242424;
+    line-height: 20px;
+    overflow: hidden;
+    padding: 1px 5px;
+    text-align: left;
+    text-overflow: ellipsis;
+    -o-text-overflow: ellipsis;
+    -ms-text-overflow: ellipsis;
+    white-space: nowrap;
+}
 
-    table col.clave { width: 120px; }
-    table col.icon { width: 25px; }
-    table col.monto { width: 115px; }
-    table col.pct { width: 60px; }
-    table col.unidad { width: 80px; }
-    table col.clave  {width: 100px; }
+table col.clave { width: 120px; }
+table col.icon { width: 25px; }
+table col.monto { width: 115px; }
+table col.pct { width: 60px; }
+table col.unidad { width: 80px; }
+table col.clave  {width: 100px; }
 
-    table tbody td input.text
-    {
-        border: none;
-        padding: 0;
-        margin: 0;
-        width: 100%;
-        background-color: transparent;
-        font-family: inherit;
-        font-size: inherit;
-        font-weight: bold;
-    }
+table tbody td input.text
+{
+    border: none;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    background-color: transparent;
+    font-family: inherit;
+    font-size: inherit;
+    font-weight: bold;
+}
 
-    table tbody .numerico
-    {
-        text-align: right;
-        padding-left: 0;
-        white-space: normal;
-    }
+table tbody .numerico
+{
+    text-align: right;
+    padding-left: 0;
+    white-space: normal;
+}
 
-    .text.is-invalid {
-        color: #dc3545;
-    }
+.text.is-invalid {
+    color: #dc3545;
+}
 
-    table tbody td input.text {
-        text-align: right;
-    }
+table tbody td input.text {
+    text-align: right;
+}
+td.editable-cell, td.editable-cell input{
+    background-color: #d0dcd0;
+}
+
+.vdp-datepicker {
+    padding: 0.2rem;
+}
 </style>
