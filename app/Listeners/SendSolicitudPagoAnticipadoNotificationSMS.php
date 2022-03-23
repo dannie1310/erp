30febe34211372847826a92b5425d5c01face865
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2022-03-23 07:43:52 -0600

    ERPHI-1175: 30m 25pmd se  implementa la solicitud de autorización de mas de un nivel para las solicitudes de pago anticipado

--- app/Listeners/SendSolicitudPagoAnticipadoNotificationSMS.php
+++ app/Listeners/SendSolicitudPagoAnticipadoNotificationSMS.php
@@ -29,6 +29,11 @@ class SendSolicitudPagoAnticipadoNotificationSMS
      */
     public function handle(SolicitudAutorizacionPagoAnticipado $event)
     {
+        $nivel_bajo_pendiente = $event->solicitud->autorizacionesRequeridas()
+            ->whereNull("id_firmante")
+            ->orderBy("nivel_requerido","desc")->pluck("nivel_requerido")->first();
+
+
         $permiso = ["autorizar_rechazar_solicitud_pago"];
 
         $usuarios_interesados_permisos = usuario::usuarioPermisoGlobal(
@@ -37,54 +42,54 @@ class SendSolicitudPagoAnticipadoNotificationSMS
 
         foreach($usuarios_interesados_permisos as $usuario_interesado_permiso)
         {
-            if($usuario_interesado_permiso->numero_celular)
-            {
-                $tokenobj = $usuario_interesado_permiso->createToken('autorizar-solicitud-pago-anticipado',['autorizar-solicitudes-pago-anticipado']);
-                $token = $tokenobj->accessToken;
-                $token_id = $tokenobj->token->id;
+            if($usuario_interesado_permiso->firmante && $usuario_interesado_permiso->firmante->nivelAutorizacion && $usuario_interesado_permiso->firmante->nivelAutorizacion->nivel == $nivel_bajo_pendiente) {
+                if ($usuario_interesado_permiso->numero_celular) {
+                    $tokenobj = $usuario_interesado_permiso->createToken('autorizar-solicitud-pago-anticipado', ['autorizar-solicitudes-pago-anticipado']);
+                    $token = $tokenobj->accessToken;
+                    $token_id = $tokenobj->token->id;
 
-                /**
-                WHATSAPP
-                 */
+                    /**
+                     * WHATSAPP
+                     */
 
-                /*$recipient = "whatsapp:" . $usuario_interesado_permiso->numero_celular;
-                $twilio_whatsapp_number = config('app.env_variables.TWILIO_WHATSAPP_NUMBER');
-                $account_sid = config('app.env_variables.TWILIO_SID');
-                $auth_token = config('app.env_variables.TWILIO_AUTH_TOKEN');
+                    /*$recipient = "whatsapp:" . $usuario_interesado_permiso->numero_celular;
+                    $twilio_whatsapp_number = config('app.env_variables.TWILIO_WHATSAPP_NUMBER');
+                    $account_sid = config('app.env_variables.TWILIO_SID');
+                    $auth_token = config('app.env_variables.TWILIO_AUTH_TOKEN');
 
-                $client = new Client($account_sid, $auth_token);
-                $client->messages->create($recipient, array(
-                    'from' => "whatsapp:$twilio_whatsapp_number",
-                    'body' => "Se le informa que ".$event->solicitud->solicitudPagoAutorizacion->usuarioRegistro->nombre_completo." ha solicitado que se autorice el pago anticipado que se describe a continuación:"
-                        ."\nProyecto: " .$event->solicitud->obra->nombre." "
-                        ."\nFolio: " .$event->solicitud->numero_folio_format." "
-                        ."\nProveedor: ".$event->solicitud->empresa->razon_social." "
-                        ."\nMonto: ".$event->solicitud->monto_format." "
-                ));*/
-
-                /**
-                SMS
-                 */
-
-                $account_sid = config('app.env_variables.TWILIO_SID');
-                $auth_token = config('app.env_variables.TWILIO_AUTH_TOKEN');
-                $client = new Client($account_sid, $auth_token);
-                $recipient = $usuario_interesado_permiso->numero_celular;
-                $twilio_number = config('app.env_variables.TWILIO_SMS_NUMBER');
-                $client->messages
-                    ->create($recipient, // to
-                        [
-                            "body" => "Se le informa que ".$event->solicitud->solicitudPagoAutorizacion->usuarioRegistro->nombre_completo." ha solicitado que se autorice el pago anticipado que se describe a continuación:"
+                    $client = new Client($account_sid, $auth_token);
+                    $client->messages->create($recipient, array(
+                        'from' => "whatsapp:$twilio_whatsapp_number",
+                        'body' => "Se le informa que ".$event->solicitud->solicitudPagoAutorizacion->usuarioRegistro->nombre_completo." ha solicitado que se autorice el pago anticipado que se describe a continuación:"
                             ."\nProyecto: " .$event->solicitud->obra->nombre." "
                             ."\nFolio: " .$event->solicitud->numero_folio_format." "
                             ."\nProveedor: ".$event->solicitud->empresa->razon_social." "
                             ."\nMonto: ".$event->solicitud->monto_format." "
+                    ));*/
 
-                            ,
-                            "from" => $twilio_number
-                        ]
-                    );
+                    /**
+                     * SMS
+                     */
 
+                    $account_sid = config('app.env_variables.TWILIO_SID');
+                    $auth_token = config('app.env_variables.TWILIO_AUTH_TOKEN');
+                    $client = new Client($account_sid, $auth_token);
+                    $recipient = $usuario_interesado_permiso->numero_celular;
+                    $twilio_number = config('app.env_variables.TWILIO_SMS_NUMBER');
+                    $client->messages
+                        ->create($recipient, // to
+                            [
+                                "body" => mb_substr("" . $event->solicitud->transaccionGeneral->usuarioRegistro->usuario . " ha solicitado que se autorice el pago anticipado:"
+                                    . "\nProyecto: " . $event->solicitud->obra->nombre . " "
+                                    . "\nFolio: " . $event->solicitud->numero_folio_format . " "
+                                    . "\nProveedor: " . $event->solicitud->empresa->razon_social . " "
+                                    . "\nMonto: " . $event->solicitud->monto_format . " ", 0, 155)
+                                ,
+                                "from" => $twilio_number
+                            ]
+                        );
+
+                }
             }
         }
     }
