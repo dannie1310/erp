995b95a6f6b0c7baacf9c514ced68f6ff5890f48
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2022-05-27 20:11:50 -0500

    ERPHI-1262: 8h se generan algunas respuestas en el chatbot / se guarda token de autorización por firmante y transacción

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\v1\SEGURIDAD_ERP\Finanzas\SolicitudPagoAutorizacionController;
+use App\Models\IGH\Usuario;
 use Illuminate\Http\Request;
 use GuzzleHttp\Exception\RequestException;
 use Twilio\Rest\Client;
@@ -12,8 +14,43 @@ class ChatBotController extends Controller
     {
         $from = $request->input('From');
         $body = $request->input('Body');
+        $body_ex = explode(" ", $body);
+        $numero_celular = str_replace("whatsapp:","", $from);
 
-        $client = new \GuzzleHttp\Client();
+        $usuario_from = Usuario::where("numero_celular","=",$numero_celular)->first();
+
+        if(!$usuario_from){
+            $this->sendWhatsAppMessage("El número celular: ".$numero_celular." no esta asociado a ningún usuario.
+			\n Por favor solicite a Soporte a Aplicaciones que le asignen el número.", $from);
+        }
+
+        switch($body_ex[0])
+        {
+            case "AUT":
+                if(!key_exists(1, $body_ex))
+                {
+                    $this->sendWhatsAppMessage("Para autorizar una transacción debe mandar el mensaje con el siguiente formato: AUT ####, donde #### es el identificador de transacción que desea autorizar.", $from);
+                }else{
+                    if(!is_numeric($body_ex[1])){
+                        $this->sendWhatsAppMessage("Para autorizar una transacción debe mandar el mensaje con el siguiente formato: AUT ####, donde #### es el identificador de transacción que desea autorizar.", $from);
+                    }
+				}
+
+
+
+                break;
+
+            case "REC":
+                break;
+
+            default:
+
+                $this->sendWhatsAppMessage("La opción que ingresó no es válida.", $from);
+
+                break;
+        }
+
+        /*$client = new \GuzzleHttp\Client();
         try {
             $response = $client->request('GET', "https://api.github.com/users/$body");
             $githubResponse = json_decode($response->getBody());
@@ -32,7 +69,10 @@ class ChatBotController extends Controller
         } catch (RequestException $th) {
             $response = json_decode($th->getResponse()->getBody());
             $this->sendWhatsAppMessage($response->message, $from);
-        }
+        }*/
+
+
+
         return;
     }
 
@@ -43,6 +83,10 @@ class ChatBotController extends Controller
      */
     public function sendWhatsAppMessage(string $message, string $recipient)
     {
+        /*$twilio_whatsapp_number = getenv('TWILIO_WHATSAPP_NUMBER');
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_AUTH_TOKEN");*/
+
         $twilio_whatsapp_number = config('app.env_variables.TWILIO_WHATSAPP_NUMBER');
         $account_sid = config('app.env_variables.TWILIO_SID');
         $auth_token = config('app.env_variables.TWILIO_AUTH_TOKEN');
--- app/Listeners/SendSolicitudPagoAnticipadoNotificationSMS.php
+++ app/Listeners/SendSolicitudPagoAnticipadoNotificationSMS.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\AperturaInvitacion;
 use App\Events\SolicitudAutorizacionPagoAnticipado;
 use App\Models\IGH\Usuario;
+use App\Models\SEGURIDAD_ERP\EsquemaAutorizacion\Token;
 use App\Models\SEGURIDAD_ERP\Notificaciones\Suscripcion;
 use App\Notifications\NotificacionInvitacionAbierta;
 use App\Notifications\NotificacionSolicitudAutorizacionPagoAnticipado;
@@ -48,12 +49,19 @@ class SendSolicitudPagoAnticipadoNotificationSMS
                     $token = $tokenobj->accessToken;
                     $token_id = $tokenobj->token->id;
 
+                    Token::create([
+                        "id_firmante"=>$usuario_interesado_permiso->firmante->id,
+                        "id_transaccion"=>$event->solicitud->transaccionGeneral->id,
+                        "token"=>$token
+                    ]);
+
                     /**
                      * WHATSAPP
                      */
 
                     //$recipient = "whatsapp:+525546347020" ;
                     //$recipient = "whatsapp:+5215518673524" ;
+                    //$recipient = "whatsapp:+5215541353541";
 
                     $recipient ="whatsapp:". $usuario_interesado_permiso->numero_celular;
                     $twilio_whatsapp_number = config('app.env_variables.TWILIO_WHATSAPP_NUMBER');
@@ -64,12 +72,21 @@ class SendSolicitudPagoAnticipadoNotificationSMS
                     $message = $client->messages->create($recipient, array(
                         'from' => "whatsapp:$twilio_whatsapp_number",
                         'body' => "Se le informa que ".$event->solicitud->transaccionGeneral->usuarioRegistro->nombre_completo." ha solicitado que se autorice el pago anticipado que se describe a continuación:"
+                            ."\nIdentificador: " .$event->solicitud->transaccionGeneral->id." "
                             ."\nProyecto: " .$event->solicitud->obra->nombre." "
                             ."\nFolio: " .$event->solicitud->numero_folio_format." "
                             ."\nProveedor: ".$event->solicitud->empresa->razon_social." "
                             ."\nMonto: ".$event->solicitud->monto_format." "
                             ."\nMotivo: ".$event->solicitud->observaciones." "
+                            ."\nAutorizar: AUT ".$event->solicitud->transaccionGeneral->id." "
+                            ."\nRechazar: REC ".$event->solicitud->transaccionGeneral->id." "
+
                     ));
+                    //print_r($message);
+
+                    /*
+                    ."\n \n Autorizar: https://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/api/solicitud-pago-anticipado/".$event->solicitud->transaccionGeneral->id."/autorizar?access_token=".$token." "
+                    */
 
 
                     /**
