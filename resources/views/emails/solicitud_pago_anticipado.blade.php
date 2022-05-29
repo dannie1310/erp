5fbbbd8ca9f61178edea2c6242e0859750f3e807
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2022-05-29 12:59:55 -0500

    ERPHI-1262: 5m se establece protocolo https en enlaces de acción de los correos para autorizar

--- resources/views/emails/solicitud_pago_anticipado.blade.php
+++ resources/views/emails/solicitud_pago_anticipado.blade.php
@@ -108,9 +108,9 @@
                     @endif
                     @if($token)
                         <br>
-                        <a href="http://{{$_SERVER['SERVER_NAME']}}:{{$_SERVER['SERVER_PORT']}}/api/solicitud-pago-anticipado/{{$solicitud->transaccionGeneral->id}}/autorizar-vista?access_token={{$token}}">[AUTORIZAR]</a>
-                        <a href="http://{{$_SERVER['SERVER_NAME']}}:{{$_SERVER['SERVER_PORT']}}/api/solicitud-pago-anticipado/{{$solicitud->transaccionGeneral->id}}/pide-motivo-rechazo?access_token={{$token}}">[RECHAZAR]</a>
-                        <a href="http://{{$_SERVER['SERVER_NAME']}}:{{$_SERVER['SERVER_PORT']}}/api/solicitud-pago-anticipado?access_token={{$token}}">[VER PENDIENTES]</a>
+                        <a href="https://{{$_SERVER['SERVER_NAME']}}:{{$_SERVER['SERVER_PORT']}}/api/solicitud-pago-anticipado/{{$solicitud->transaccionGeneral->id}}/autorizar-vista?access_token={{$token}}">[AUTORIZAR]</a>
+                        <a href="https://{{$_SERVER['SERVER_NAME']}}:{{$_SERVER['SERVER_PORT']}}/api/solicitud-pago-anticipado/{{$solicitud->transaccionGeneral->id}}/pide-motivo-rechazo?access_token={{$token}}">[RECHAZAR]</a>
+                        <a href="https://{{$_SERVER['SERVER_NAME']}}:{{$_SERVER['SERVER_PORT']}}/api/solicitud-pago-anticipado?access_token={{$token}}">[VER PENDIENTES]</a>
                     @endif
 
                 </span>
