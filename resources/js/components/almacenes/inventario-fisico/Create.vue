4f8e7c9ea56ae2ddfe6669fecaaeac5e76dbc8c4
DBenitezC <dbenitezc@grupohi.mx>
2022-11-30 23:35:17 -0600

    ERPHI-1401: Se agrega el multiselect de almacenes para crear el inventario fisico

--- resources/js/components/almacenes/inventario-fisico/Create.vue
+++ resources/js/components/almacenes/inventario-fisico/Create.vue
@@ -1,11 +1,11 @@
 <template>
     <span>
-        <button @click="init" v-if="$root.can('iniciar_inventario_fisico')" class="btn btn-app btn-info float-right" :disabled="cargando">
+        <button @click="init" v-if="$root.can('iniciar_inventario_fisico')" class="btn btn-app float-right" :disabled="cargando">
             <i class="fa fa-spin fa-spinner" v-if="cargando"></i>
             <i class="fa fa-plus" v-else></i>
             Iniciar Inventario Físico
         </button>
-                 <div class="modal fade" ref="modal" role="dialog" aria-hidden="true">
+        <div class="modal fade" ref="modal" role="dialog" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-md" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -15,10 +15,14 @@
                         </button>
                     </div>
                     <form role="form" @submit.prevent="validate">
-                        <div class="modal-body">
+                        <div class="modal-body" v-if="cargando">
+                            <div class="spinner-border text-success" role="status">
+                                <span class="sr-only">Cargando...</span>
+                            </div>
+                        </div>
+                        <div class="modal-body" v-else>
                             <div class="row">
-                                <div class="col-md-2"></div>
-                                    <div class="col-md-5">
+                                <div class="col-md-5">
                                     <div class="form-group row error-content">
                                         <label for="id_tipo" class="col-sm-2 col-form-label">Tipo: </label>
                                         <div class="col-sm-10" align="right">
@@ -39,11 +43,11 @@
                                         </div>
                                     </div>
                                 </div>
-                                <!--Inventario-->
-                                    <div class="col-md-5">
+                                <div class="col-md-2"></div>
+                                <div class="col-md-5">
                                     <div class="form-group row error-content">
-                                        <label for="inventario" class="col-sm-5 col-form-label">Porcentaje:</label>
-                                        <div class="col-sm-5">
+                                        <label for="inventario" class="col-md-5 col-form-label">Porcentaje:</label>
+                                        <div class="col-md-5">
                                             <select
                                                 :disabled="id_tipo!=2"
                                                 type="text"
@@ -63,12 +67,25 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <multiselect v-model="value" :options="options"></multiselect>
+                                <div class="col-md-12">
+                                    <div class="form-group row error-content">
+                                        <label class="col-md-4 col-form-label">Almacenes: </label>
+                                        <div class="col-md-8">
+                                        <treeselect v-model="almacenes_seleccionados"
+                                                    :multiple="true"
+                                                    :options="almacenes"
+                                                    data-vv-as="Almacenes"
+                                                    placeholder="Seleccione los almacenes deseados">
+                                            <div slot="value-label" slot-scope="{ node }">{{ node.raw.customLabel }}</div>
+                                        </treeselect>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="modal-footer">
                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                            <button type="submit" class="btn btn-primary">Iniciar</button>
+                            <button type="submit" class="btn btn-primary" v-if="!cargando">Iniciar</button>
                         </div>
                     </form>
                 </div><!-- /.modal-content -->
@@ -78,10 +95,8 @@
 </template>
 
 <script>
-    import Multiselect from 'vue-multiselect',
     export default {
         name: "inventario-fisico-create",
-        components : {Multiselect},
         data() {
             return {
                 cargando: false,
@@ -97,12 +112,13 @@
                     2: "Parcial"
                 },
                 id_tipo:'',
-                form: {inventario:''},
-                value: null,
-                options: ['list', 'of', 'options']
+                form: {inventario: '', almacenes: []},
+                almacenes: [],
+                almacenes_seleccionados: []
             }
         },
         mounted(){
+            this.getAlmacenes();
         },
         methods:{
             init() {
@@ -111,30 +127,54 @@
                 this.cargando = false;
                 this.id_tipo='';
                 this.form.inventario='';
-
+                this.form.almacenes=[];
+                this.almacenes_seleccionados= [];
+                if(this.almacenes == []) {
+                    this.getAlmacenes();
+                }
+            },
+            getAlmacenes() {
+                this.$store.commit('cadeco/almacen/SET_ALMACENES', []);
+                this.cargando = true;
+                return this.$store.dispatch('cadeco/almacen/index', {
+                    params: {sort: 'descripcion', order: 'asc', scope: 'tipoMaterialYHerramienta' }
+                })
+                    .then(data => {
+                        this.almacenes = data.data
+                        this.almacenesAcomodar();
+                    })
+                    .finally(()=>{
+                        this.cargando = false;
+                    })
+            },
+            almacenesAcomodar () {
+                this.almacenes = this.almacenes.map(i => ({
+                    id: i.id,
+                    label: `${i.descripcion}`,
+                    customLabel: `${i.descripcion}`,
+                }));
             },
             store() {
-                if(this.id_tipo==1){
-                return this.$store.dispatch('almacenes/inventario-fisico/store', this.dato)
-                    .then(data => {
-                        this.$emit('created', data);
-                    }).finally( ()=>{
-                        this.cargando = false;
-                        $(this.$refs.modal).modal('hide');
-                    });
-                    console.log(this.id_tipo)
-                }else{
+                if (this.id_tipo == 1) {
+                    var datos = {total: true, almacenes: this.almacenes_seleccionados};
+                    return this.$store.dispatch('almacenes/inventario-fisico/store', datos)
+                        .then(data => {
+                            this.$emit('created', data);
+                        }).finally(() => {
+                            this.cargando = false;
+                            $(this.$refs.modal).modal('hide');
+                        });
+                } else {
+                    this.form.almacenes = this.almacenes_seleccionados;
                     return this.$store.dispatch('almacenes/inventario-fisico/store', this.form)
                         .then(data => {
                             this.$emit('created', data);
-                        }).finally( ()=>{
+                        }).finally(() => {
                             this.cargando = false;
                             $(this.$refs.modal).modal('hide');
                         });
                 }
-
-
-        },
+            },
             validate() {
                 this.$validator.validate().then(result => {
                     if (result) {
@@ -145,12 +185,3 @@
         }
     }
 </script>
-
-<style>
-    .error-label {
-        width: 100%;
-        margin-top: 0.25rem;
-        font-size: 80%;
-        color: #dc3545;
-    }
-</style>
