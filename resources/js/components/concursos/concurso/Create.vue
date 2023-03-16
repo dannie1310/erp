0e1e1895406ec06eac88b2e06010eb261fed1945
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2023-03-16 16:07:07 -0600

    ERPHI-1528: 10m se omite agregar a los participantes durante el registro del concurso

--- resources/js/components/concursos/concurso/Create.vue
+++ resources/js/components/concursos/concurso/Create.vue
@@ -74,54 +74,6 @@
 
 
                 <br />
-                <div class="row">
-                    <div  class="col-12">
-                        <div class="table-responsive">
-                            <table class="table  table-sm table-bordered" style="font-size: 11px">
-                                <thead>
-                                    <tr>
-                                        <th class="encabezado index_corto">
-                                            #
-                                        </th>
-                                        <th class="encabezado">
-                                            Participante
-                                        </th>
-                                        <th class="encabezado c100">
-                                            Monto
-                                        </th>
-                                        <th class="encabezado c60">
-                                            Es Hermes
-                                        </th>
-                                        <th class="encabezado icono">
-                                            <button type="button" class="btn btn-sm btn-outline-success" v-on:click="agregar()">
-                                                <i class="fa fa-plus"></i>
-                                            </button>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="(participante, i) in this.participantes">
-                                        <td>{{i+1}}</td>
-                                        <td>
-                                           {{participante.nombre}}
-                                        </td>
-                                        <td style="text-align: right;">
-                                            {{parseFloat(participante.monto).formatMoney(2,'.',',')}}
-                                        </td>
-                                        <td style="text-align: center">
-                                            <input type="checkbox" id="es_hermes" v-model="participante.es_hermes" disabled>
-                                        </td>
-                                        <td style="text-align: center">
-                                            <button type="button" class="btn btn-sm btn-outline-danger" v-on:click="quitar(i)">
-                                                <i class="fa fa-trash"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="modal-footer">
                 <div class="pull-right">
@@ -132,73 +84,6 @@
                         <i class="fa fa-save"></i>
                         Guardar
                     </button>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade" ref="modal1" role="dialog">
-            <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
-                <div class="modal-content" >
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle"> <i class="fa fa-plus"></i>&nbsp;AGREGAR PARTICIPANTE</h5>
-                        <button type="button" class="close" @click="cerrar" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="nombre">Nombre Participante:</label>
-                                        <input class="form-control"
-                                                placeholder="Nombre del Participante"
-                                                name="nombre"
-                                                id="nombre"
-                                                v-validate="{max: 255}"
-                                                data-vv-as="Nombre del Participante"
-                                                v-model="participante.nombre"
-                                                :class="{'is-invalid': errors.has('nombre')}">
-                                        <div class="invalid-feedback" v-show="errors.has('nombre')">{{ errors.first('nombre') }}</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="nombre">Monto:</label>
-                                        <input
-                                                type="number"
-                                                name="monto"
-                                                v-model="participante.monto"
-                                                data-vv-as="Monto"
-                                                v-validate="{min_value: 0.1, regex: /^[0-9]\d*(\.\d{0,2})?$/}"
-                                                class="form-control"
-                                                :class="{'is-invalid': errors.has(`monto`)}"
-                                                style="text-align: right"
-                                                id="monto"
-                                                placeholder="Monto">
-                                        <div class="invalid-feedback"
-                                                v-show="errors.has(`monto`)">{{ errors.first(`monto`) }}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row" v-if="this.es_hermes_seleccionado == 0">
-                                <div class="col-md-12" >
-                                    <div class="form-group">
-                                        <input type="checkbox" id="es_hermes" v-model="participante.es_hermes" >
-                                        <label for="nombre">&nbsp;Es Hermes</label>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" @click="cerrar"><i class="fa fa-close"></i>Cerrar</button>
-                        <button type="button" @click="guardar_participante" class="btn btn-primary">
-                            <i class="fa fa-plus"></i>Agregar
-                        </button>
-                    </div>
                 </div>
             </div>
         </div>
@@ -213,7 +98,6 @@
         components: {
             Datepicker},
         mounted() {
-            this.$validat
             this.fecha = new Date();
             this.$validator.reset();
             this.fechasDeshabilitadas.from = new Date();
@@ -317,6 +201,9 @@
                 })
                     .then(data=> {
                         this.$store.commit('concursos/concurso/SET_CONCURSO', data);
+                    })
+                    .catch(error => {
+                        reject(error);
                     })
                     .finally(() => {
                         this.$router.push({name: 'concurso-edit', params: {id: this.concurso_registrado.id}});
