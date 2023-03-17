277e6bf8928c1df5958b4e212d7b54a7af53ad1e
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2023-03-17 14:44:58 -0600

    ERPHI-1546: 30m van 1h 39m se corrige funcion de formateo para presentar los decimales

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -41,69 +41,50 @@ Number.prototype.formatea = function(c, d, t) {
     return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
 
-
-function quitacomas(valor)
-{
-    let dato = valor.replace(/,/g,'');
-    return dato;
-}
-
-String.prototype.formatearkeyUp =function ()
-{
-    fin1= this;
-    if(fin1=="")
-    {
-        campo="";
+String.prototype.formatearkeyUp =function () {
+    fin1 = this;
+    if (fin1 == "") {
+        campo = "";
         return campo;
-    }else if(fin1=="."){
-        campo=".";
+    } else if (fin1 == ".") {
+        campo = ".";
         return campo;
-    }
-    else
-    {
-        var index=fin1.indexOf(".");
+    } else {
+        var index = fin1.indexOf(".");
 
-        if(index==-1)
-        {
-            fin=quitacomas(fin1);
+        if (index == -1) {
+            fin = fin1.replace(/,/g, '');
             resultado1 = parseFloat(fin).toString();
             resultados = resultado1.split(".");
-            var cadena="";
-            cont=1
-            for(m=resultados[0].length-1; m>=0; m--)
-            {
+            var cadena = "";
+            cont = 1
+            for (m = resultados[0].length - 1; m >= 0; m--) {
                 cadena = resultados[0].charAt(m) + cadena
-                cont%3 == 0 && m >0 ? cadena = "," + cadena : cadena = cadena
-                cont== 3 ? cont = 1 :cont++
+                cont % 3 == 0 && m > 0 ? cadena = "," + cadena : cadena = cadena
+                cont == 3 ? cont = 1 : cont++
             }
-            if(resultados[1])
-            {
-                campo=cadena+"."+resultado2[1];
+            if (resultados[1]) {
+                campo = cadena + "." + resultados[1];
+                return campo;
+            } else {
+                campo = cadena;
                 return campo;
             }
-            else
-            {
-                campo= cadena;
-                return campo;
-            }
-        }
-        else
-        {
-
-            fin=quitacomas(fin1);
-            resultado1 = parseFloat(fin).toFixed(3).toString();
-            resultados = resultado1.split(".");
-            var cadena="";
-            cont=1
-            for(m=resultados[0].length-1; m>=0; m--)
-            {
+        } else {
+            fin = fin1.replace(/,/g, '');
+            resultados = fin.split(".");
+            var cadena = "";
+            cont = 1
+            for (m = resultados[0].length - 1; m >= 0; m--) {
                 cadena = resultados[0].charAt(m) + cadena
-                cont%3 == 0 && m >0 ? cadena = "," + cadena : cadena = cadena
-                cont== 3 ? cont = 1 :cont++
+                cont % 3 == 0 && m > 0 ? cadena = "," + cadena : cadena = cadena
+                cont == 3 ? cont = 1 : cont++
             }
-            if(resultados[1])
-            {
-                campo=cadena+"."+resultado2[1];
+            if (resultados[1]) {
+                campo = cadena + "." + resultados[1];
+                return campo;
+            }else {
+                campo = cadena + ".";
                 return campo;
             }
         }
