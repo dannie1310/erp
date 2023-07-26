9af2d047a9fe52d9bc8b5b06b6d5b2dcb90cee05
dbenitezc <dbenitezc@grupohi.mx>
2023-07-26 12:55:13 -0600

    ERPHI-1749: Se corrige error al crear familia con dos puntos extra

--- app/Models/CADECO/Material.php
+++ app/Models/CADECO/Material.php
@@ -390,7 +390,7 @@ class Material extends Model
                      'tipo_material' => $familia->tipo_material,
                      'descripcion' => $familia->descripcion .'1'
                  ]);
-                $nivel_completo = $nueva_familia->nivel.'.000.';
+                $nivel_completo = $nueva_familia->nivel.'000.';
             }else{
                 $nueva_familia = Familia::where('descripcion','=',$familia->descripcion.$familia->consecutivo_familia)->first();
                 $hijos_familia = self::where('tipo_material','=',$nueva_familia->tipo_material)->where('nivel','LIKE',$nueva_familia->nivel.'%')->whereRaw('LEN(nivel) = 8')->orderBy('nivel', 'asc')->get()->pluck('nivel');
