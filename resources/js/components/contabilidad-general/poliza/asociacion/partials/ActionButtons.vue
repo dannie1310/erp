6549f82a893ffb1ea897d47529c549408c62163d
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2023-08-01 14:32:42 -0600

    ERPHI-1744: 1m corregir ruta de consulta de póliza en action buttons de index de pólizs para asociación

--- resources/js/components/contabilidad-general/poliza/asociacion/partials/ActionButtons.vue
+++ resources/js/components/contabilidad-general/poliza/asociacion/partials/ActionButtons.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="btn-group">
-        <router-link  :to="{ name: 'poliza-contpaq-show', params: {id: value.id, id_empresa: this.value.id_empresa}}" v-if="$root.can('consultar_poliza',1)" type="button" class="btn btn-sm btn-outline-secondary" title="Ver">
+        <router-link  :to="{ name: 'poliza-contpaq-asociacion-show', params: {id: value.id, id_empresa: this.value.id_empresa}}" v-if="$root.can('consultar_poliza',1)" type="button" class="btn btn-sm btn-outline-secondary" title="Ver">
             <i class="fa fa-eye"></i>
         </router-link>
         <router-link  :to="{ name: 'poliza-contpaq-asociacion-asociar-cfdi', params: {id: this.value.id, id_empresa: this.value.id_empresa}}" v-if="$root.can('asociar-cfdi-a-poliza-contpaq',1)" type="button" class="btn btn-sm btn-outline-warning" title="Asociar CFDI">
