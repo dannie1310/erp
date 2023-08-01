29c4a224804d5a0eb8463465328917e3d9ff1e7d
Elizabeth Martinez Solano <emartinez@grupohi.mx>
2023-08-01 15:10:32 -0600

    ERPHI-1744: 51m T 20h 57m se complementa middleware para validar acceso a empresa por asignación de contpaq

--- app/Http/Middleware/CheckAccesoEmpresaContpaq.php
+++ app/Http/Middleware/CheckAccesoEmpresaContpaq.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\CTPQ\GeneralesSQL\Usuario;
+use App\Models\SEGURIDAD_ERP\Contabilidad\Empresa;
 use App\Models\SEGURIDAD_ERP\Contabilidad\EmpresaUsuario;
 use Closure;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -18,11 +20,30 @@ class CheckAccesoEmpresaContpaq
     public function handle($request, Closure $next)
     {
         $id_empresa = $request->id_empresa?$request->id_empresa:$request->id;
+        if(!$id_empresa>0)
+        {
+            throw new BadRequestHttpException('No se detecta un identificador de empresa válido en la petición');
+        }
+
         $id_usuario = auth()->user()->idusuario;
         $empresa_usuario = EmpresaUsuario::where("id_empresa","=",$id_empresa)
             ->where("id_usuario","=",$id_usuario)->get()->count();
 
-        if($empresa_usuario == 1){
+        $usuario = strtoupper(auth()->user()->usuario);
+        $empresa = Empresa::find($id_empresa);
+        $id_empresa_contpaq = $empresa ?$empresa->IdEmpresaContpaq:0;
+
+        $id_usuario_contpaq = Usuario::where("codigo","=",$usuario)
+            ->where("EsBaja","=",0)
+            ->pluck("Id")
+            ->first();
+
+        $empresa_contpaq_usuario = \App\Models\CTPQ\GeneralesSQL\EmpresaUsuario::where("IdUsuario","=",$id_usuario_contpaq)
+        ->where("IdEmpresa","=",$id_empresa_contpaq)
+        ->get()
+        ->count();
+
+        if($empresa_usuario > 0 || $empresa_contpaq_usuario >0){
             return $next($request);
         }
         throw new BadRequestHttpException('No tiene permitido el acceso a esta empresa de Contpaq');
