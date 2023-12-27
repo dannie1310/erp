a890430e1ed9996624abf52211f5d42f3b1332ec
dbenitezc <dbenitezc@grupohi.mx>
2023-12-27 17:07:51 -0600

    ERPHI-1813: Se cambia el encabezado de proyecto a proveedor en la relacion de gastos

--- resources/js/components/control-recursos/gastos/relacion-gastos/partials/TablaDatosResumen.vue
+++ resources/js/components/control-recursos/gastos/relacion-gastos/partials/TablaDatosResumen.vue
@@ -28,7 +28,7 @@
                     </tr>
                     <tr>
                         <th class="encabezado  c130" colspan="2">
-                           Proyecto
+                           Proveedor
                         </th>
                         <th class="encabezado">
                             Estado
