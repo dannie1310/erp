6b29d32859e3c902a1c03b9d56f9a861ca4cd676
dbenitezc <dbenitezc@grupohi.mx>
2024-10-22 23:31:16 -0600

    ERPHI-1945: Se corrige error cuando aun no se selecciona la obra

--- resources/js/components/nominas/poliza-nomina/SeleccionarEmpresa.vue
+++ resources/js/components/nominas/poliza-nomina/SeleccionarEmpresa.vue
@@ -25,7 +25,7 @@
                 </div>
             </div>
             <div class="col-md-2">
-                <button @click="conectar" class="btn btn-primary float-left">
+                <button @click="conectar" class="btn btn-primary float-left" :disabled="id_empresa == '' ? true : false">
                     <i class="fa fa-plug"></i> Conectar
                 </button>
             </div>
