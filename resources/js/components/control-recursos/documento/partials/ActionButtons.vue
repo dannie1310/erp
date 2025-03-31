59460d8dba8707aa927d5b41056994a68dcde7ac
dbenitezc <dbenitezc@grupohi.mx>
2025-03-31 12:19:31 -0600

    ERPHI-1983: cambio para validar poder descargar y enviar xml a ifs

--- resources/js/components/control-recursos/documento/partials/ActionButtons.vue
+++ resources/js/components/control-recursos/documento/partials/ActionButtons.vue
@@ -6,22 +6,20 @@
         <router-link v-else  :to="{ name: 'documento-recurso-show', params: {id: value.id}}" type="button" class="btn btn-sm btn-outline-secondary" title="Consultar">
             <i class="fa fa-eye"></i>
         </router-link>
-
         <router-link v-if="value.con_cfdi && value.edit" :to="{ name: 'factura-recurso-edit', params: {id: value.id}}" type="button" class="btn btn-sm btn-outline-info" title="Editar">
             <i class="fa fa-pencil"></i>
         </router-link>
         <router-link v-if="!value.con_cfdi && value.edit" :to="{ name: 'documento-recurso-edit', params: {id: value.id}}" type="button" class="btn btn-sm btn-outline-info" title="Editar">
             <i class="fa fa-pencil"></i>
         </router-link>
-
         <router-link v-if="value.con_cfdi && value.delete" :to="{ name: 'factura-recurso-delete', params: {id: value.id}}" type="button" class="btn btn-sm btn-outline-danger" title="Eliminar">
             <i class="fa fa-trash"></i>
         </router-link>
         <router-link v-if="!value.con_cfdi && value.delete" :to="{ name: 'documento-recurso-delete', params: {id: value.id}}" type="button" class="btn btn-sm btn-outline-danger" title="Eliminar">
             <i class="fa fa-trash"></i>
         </router-link>
-        <DescargaXML v-if="value.xml_ifs" v-bind:id="value.id" />
-        <button @click="correo" v-if="value.correo_xml_ifs" type="button" class="btn btn-sm btn-outline-success" title="Envio Correo de XML">
+        <DescargaXML v-if="value.xml_ifs && value.con_cfdi" v-bind:id="value.id" />
+        <button @click="correo" v-if="value.correo_xml_ifs && value.con_cfdi" type="button" class="btn btn-sm btn-outline-success" title="Envio Correo de XML">
             <i class="fa fa-envelope"></i>
         </button>
     </div>
